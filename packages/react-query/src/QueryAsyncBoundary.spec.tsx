eefef7ba150f78edaa52079bbaa30ededf6c72fd
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-02-19 20:26:21 +0900

    test(react, react-query): remove unnecessary code

--- packages/react-query/src/QueryAsyncBoundary.spec.tsx
+++ packages/react-query/src/QueryAsyncBoundary.spec.tsx
@@ -1,11 +1,5 @@
 import { render } from '@testing-library/react'
 
-export const sum = (a: number, b: number) => a + b
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
-})
-
 describe('test', () => {
   it('should test', () => {
     const TEST_TEXT = 'suspensive'
