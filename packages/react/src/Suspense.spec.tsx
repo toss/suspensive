2bedd037b3d24377f749f7f3ffd9c09bdae2e614
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-06-11 23:32:32 +0900

    test(react): add test case for Suspense (#75)
    * test(react): add test case for Suspense
    
    * test(react): code reviewed

---
+++ packages/react/src/Suspense.spec.tsx
@@ -0,0 +1,80 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import { Suspense } from './Suspense'
+
+const ms = 1000
+const TEXT = 'Child'
+const FALLBACK = 'Fallback'
+
+const Throw = () => {
+  throw new Promise((resolve) => resolve('resolved'))
+}
+
+describe('Suspense', () => {
+  let needThrow = true
+  beforeEach(() => {
+    needThrow = true
+  })
+  const ThrowDuring = (props: { ms: number }) => {
+    if (needThrow) {
+      throw new Promise((resolve) => setTimeout(() => resolve('resolved'), props.ms))
+    }
+    return <></>
+  }
+
+  it('should render the children if nothing to suspend', async () => {
+    render(<Suspense fallback={FALLBACK}>{TEXT}</Suspense>)
+    expect(screen.queryByText(TEXT)).toBeInTheDocument()
+    expect(screen.queryByText(FALLBACK)).not.toBeInTheDocument()
+  })
+  it('should render the fallback if something to suspend in children', async () => {
+    render(
+      <Suspense fallback={FALLBACK}>
+        {TEXT}
+        <Throw />
+      </Suspense>
+    )
+    expect(screen.queryByText(FALLBACK)).toBeInTheDocument()
+    expect(screen.queryByText(TEXT)).not.toBeInTheDocument()
+  })
+  it('should render the children after the suspense', async () => {
+    jest.useFakeTimers()
+    render(
+      <Suspense>
+        {TEXT}
+        <ThrowDuring ms={ms} />
+      </Suspense>
+    )
+    expect(screen.queryByText(TEXT)).not.toBeInTheDocument()
+    needThrow = false
+    jest.advanceTimersByTime(ms)
+    await waitFor(() => expect(screen.queryByText(TEXT)).toBeInTheDocument())
+  })
+  it('.CSROnly should render the fallback during suspense', () => {
+    render(
+      <Suspense.CSROnly fallback={FALLBACK}>
+        {TEXT}
+        <ThrowDuring ms={ms} />
+      </Suspense.CSROnly>
+    )
+    expect(screen.queryByText(FALLBACK)).toBeInTheDocument()
+    expect(screen.queryByText(TEXT)).not.toBeInTheDocument()
+  })
+  it('.CSROnly should render the children after the suspense', async () => {
+    jest.useFakeTimers()
+    render(
+      <Suspense.CSROnly fallback={FALLBACK}>
+        {TEXT}
+        <ThrowDuring ms={ms} />
+      </Suspense.CSROnly>
+    )
+    needThrow = false
+    jest.advanceTimersByTime(ms)
+    await waitFor(() => expect(screen.queryByText(TEXT)).toBeInTheDocument())
+    expect(screen.queryByText(FALLBACK)).not.toBeInTheDocument()
+  })
+  it('.CSROnly should render the children if nothing to suspend in children', async () => {
+    render(<Suspense.CSROnly fallback={FALLBACK}>{TEXT}</Suspense.CSROnly>)
+    expect(screen.queryByText(FALLBACK)).not.toBeInTheDocument()
+    expect(screen.queryByText(TEXT)).toBeInTheDocument()
+  })
+})
--- packages/react/src/SuspensiveProvider.spec.tsx
+++ packages/react/src/SuspensiveProvider.spec.tsx
@@ -1,9 +1,15 @@
-import { act, render, screen, waitFor } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { Delay } from './Delay'
+import { Suspense } from './Suspense'
 import { SuspensiveConfigs, SuspensiveProvider } from './SuspensiveProvider'
 
 const ms = 1000
 const TEXT = 'Child'
+const FALLBACK = 'Fallback'
+
+const Throw = () => {
+  throw new Promise((resolve) => resolve('resolved'))
+}
 
 describe('SuspensiveProvider', () => {
   it('should provide default ms prop of Delay', async () => {
@@ -14,7 +20,7 @@ describe('SuspensiveProvider', () => {
       </SuspensiveProvider>
     )
     expect(screen.queryByText(TEXT)).not.toBeInTheDocument()
-    act(() => jest.advanceTimersByTime(ms))
+    jest.advanceTimersByTime(ms)
     await waitFor(() => expect(screen.queryByText(TEXT)).toBeInTheDocument())
   })
   it('should accept configs with nothing about Delay', () => {
@@ -40,5 +46,38 @@ describe('SuspensiveProvider', () => {
       </SuspensiveProvider>
     )
     expect(screen.queryByText(TEXT)).toBeInTheDocument()
+  })
+
+  it('should accept defaultOptions.suspense.fallback to setup default fallback of Suspense. If Suspense accepted no fallback, Suspense should use default fallback', async () => {
+    render(
+      <SuspensiveProvider configs={new SuspensiveConfigs({ defaultOptions: { suspense: { fallback: FALLBACK } } })}>
+        <Suspense>
+          <Throw />
+        </Suspense>
+      </SuspensiveProvider>
+    )
+    expect(screen.queryByText(FALLBACK)).toBeInTheDocument()
+  })
+  it('should accept defaultOptions.suspense.fallback to setup default fallback of Suspense. If Suspense accepted local fallback, Suspense should ignore default fallback and show it', async () => {
+    const LOCAL_FALLBACK = 'LOCAL_FALLBACK'
+    render(
+      <SuspensiveProvider configs={new SuspensiveConfigs({ defaultOptions: { suspense: { fallback: FALLBACK } } })}>
+        <Suspense fallback={LOCAL_FALLBACK}>
+          <Throw />
+        </Suspense>
+      </SuspensiveProvider>
+    )
+    expect(screen.queryByText(FALLBACK)).not.toBeInTheDocument()
+    expect(screen.queryByText(LOCAL_FALLBACK)).toBeInTheDocument()
+  })
+  it('should accept defaultOptions.suspense.fallback to setup default fallback of Suspense. If Suspense accepted local fallback as null, Suspense should ignore default fallback. even though local fallback is nullish', async () => {
+    render(
+      <SuspensiveProvider configs={new SuspensiveConfigs({ defaultOptions: { suspense: { fallback: FALLBACK } } })}>
+        <Suspense fallback={null}>
+          <Throw />
+        </Suspense>
+      </SuspensiveProvider>
+    )
+    expect(screen.queryByText(FALLBACK)).not.toBeInTheDocument()
   })
 })
