26a46ca89bb1451d2debd93359b30bfc5630d12b
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-06-12 06:09:41 +0900

    refactor(react): remove repetitive code, naming intuitively with adding test case for withSuspense

--- packages/react/src/withDelay.spec.tsx
+++ packages/react/src/withDelay.spec.tsx
@@ -1,16 +1,15 @@
 import { render, act, screen, waitFor } from '@testing-library/react'
 import { withDelay } from './withDelay'
+import { MS_100, TEXT } from './test-utils'
 
-const ms = 1000
-const TEXT = 'Child'
+const TEXTAfterDelay100ms = withDelay(() => <>{TEXT}</>, { ms: MS_100 })
 
 describe('withDelay', () => {
   it('renders the children after the delay with component', async () => {
     jest.useFakeTimers()
-    const Wrapped = withDelay(() => <>{TEXT}</>, { ms })
-    render(<Wrapped />)
+    render(<TEXTAfterDelay100ms />)
     expect(screen.queryByText(TEXT)).not.toBeInTheDocument()
-    act(() => jest.advanceTimersByTime(ms))
+    act(() => jest.advanceTimersByTime(MS_100))
     await waitFor(() => expect(screen.queryByText(TEXT)).toBeInTheDocument())
   })
 })
