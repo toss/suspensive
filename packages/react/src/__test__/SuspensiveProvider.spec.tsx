8f75c300789bef805ae6906ce35f32641182133b
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-09-03 04:46:16 +0900

    test(react): update test code for SuspensiveProvider

--- packages/react/src/__test__/SuspensiveProvider.spec.tsx
+++ packages/react/src/__test__/SuspensiveProvider.spec.tsx
@@ -1,7 +1,7 @@
 import { act, render, screen, waitFor } from '@testing-library/react'
 import { Delay } from '../Delay'
 import { Suspense } from '../Suspense'
-import { SuspensiveConfigs, SuspensiveProvider } from '../SuspensiveProvider'
+import { Suspensive, SuspensiveProvider } from '../SuspensiveProvider'
 import { FALLBACK, MS_100, Suspend, TEXT } from './utils'
 
 const FALLBACK_GLOBAL = 'FALLBACK_GLOBAL'
@@ -10,7 +10,7 @@ describe('SuspensiveProvider', () => {
   it('should provide default ms prop of Delay', async () => {
     jest.useFakeTimers()
     render(
-      <SuspensiveProvider configs={new SuspensiveConfigs({ defaultOptions: { delay: { ms: MS_100 } } })}>
+      <SuspensiveProvider value={new Suspensive({ defaultOptions: { delay: { ms: MS_100 } } })}>
         <Delay>{TEXT}</Delay>
       </SuspensiveProvider>
     )
@@ -18,25 +18,25 @@ describe('SuspensiveProvider', () => {
     act(() => jest.advanceTimersByTime(MS_100))
     await waitFor(() => expect(screen.queryByText(TEXT)).toBeInTheDocument())
   })
-  it('should accept configs with nothing about Delay', () => {
+  it('should accept suspensive value with nothing about Delay', () => {
     render(
-      <SuspensiveProvider configs={new SuspensiveConfigs({ defaultOptions: {} })}>
+      <SuspensiveProvider value={new Suspensive({ defaultOptions: {} })}>
         <Delay>{TEXT}</Delay>
       </SuspensiveProvider>
     )
     expect(screen.queryByText(TEXT)).toBeInTheDocument()
   })
-  it('should accept empty configs', () => {
+  it('should accept empty suspensive value', () => {
     render(
-      <SuspensiveProvider configs={new SuspensiveConfigs({})}>
+      <SuspensiveProvider value={new Suspensive({})}>
         <Delay>{TEXT}</Delay>
       </SuspensiveProvider>
     )
     expect(screen.queryByText(TEXT)).toBeInTheDocument()
   })
-  it('should accept no configs', () => {
+  it('should accept no suspensive value', () => {
     render(
-      <SuspensiveProvider configs={new SuspensiveConfigs()}>
+      <SuspensiveProvider value={new Suspensive()}>
         <Delay>{TEXT}</Delay>
       </SuspensiveProvider>
     )
@@ -45,9 +45,7 @@ describe('SuspensiveProvider', () => {
 
   it('should accept defaultOptions.suspense.fallback to setup default fallback of Suspense. If Suspense accepted no fallback, Suspense should use default fallback', async () => {
     render(
-      <SuspensiveProvider
-        configs={new SuspensiveConfigs({ defaultOptions: { suspense: { fallback: FALLBACK_GLOBAL } } })}
-      >
+      <SuspensiveProvider value={new Suspensive({ defaultOptions: { suspense: { fallback: FALLBACK_GLOBAL } } })}>
         <Suspense>
           <Suspend during={Infinity} />
         </Suspense>
@@ -57,9 +55,7 @@ describe('SuspensiveProvider', () => {
   })
   it('should accept defaultOptions.suspense.fallback to setup default fallback of Suspense. If Suspense accepted local fallback, Suspense should ignore default fallback and show it', async () => {
     render(
-      <SuspensiveProvider
-        configs={new SuspensiveConfigs({ defaultOptions: { suspense: { fallback: FALLBACK_GLOBAL } } })}
-      >
+      <SuspensiveProvider value={new Suspensive({ defaultOptions: { suspense: { fallback: FALLBACK_GLOBAL } } })}>
         <Suspense fallback={FALLBACK}>
           <Suspend during={Infinity} />
         </Suspense>
@@ -70,9 +66,7 @@ describe('SuspensiveProvider', () => {
   })
   it('should accept defaultOptions.suspense.fallback to setup default fallback of Suspense. If Suspense accepted local fallback as null, Suspense should ignore default fallback. even though local fallback is nullish', async () => {
     render(
-      <SuspensiveProvider
-        configs={new SuspensiveConfigs({ defaultOptions: { suspense: { fallback: FALLBACK_GLOBAL } } })}
-      >
+      <SuspensiveProvider value={new Suspensive({ defaultOptions: { suspense: { fallback: FALLBACK_GLOBAL } } })}>
         <Suspense fallback={null}>
           <Suspend during={Infinity} />
         </Suspense>
