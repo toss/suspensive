f634b748d4f2ad89105d2fc1527f1e9b4fbc4b94
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-09-04 18:35:01 +0900

    test(react-query): update test code initialization

--- packages/react-query/src/__test__/QueryErrorBoundary.spec.tsx
+++ packages/react-query/src/__test__/QueryErrorBoundary.spec.tsx
@@ -1,10 +1,14 @@
 import { render } from '@testing-library/react'
 import { QueryErrorBoundary } from '../QueryErrorBoundary'
 
-describe('test', () => {
-  it('should test', () => {
+describe('QueryErrorBoundary', () => {
+  it('should be tested', () => {
     const TEST_TEXT = 'suspensive'
-    const fixture = render(<QueryErrorBoundary>{TEST_TEXT}</QueryErrorBoundary>)
+    const fixture = render(
+      <QueryErrorBoundary fallback={(caught) => <button onClick={caught.reset}>reset</button>}>
+        {TEST_TEXT}
+      </QueryErrorBoundary>
+    )
 
     expect(fixture.getByText(TEST_TEXT)).toBeInTheDocument()
   })
