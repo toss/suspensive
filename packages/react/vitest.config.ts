aa53e76043dcf659538a7c18216dfe266b627e35
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-09-06 00:13:36 +0900

    Update packages/react/vitest.config.ts
    Co-authored-by: 박찬혁 <pgg6713@gmail.com>

--- packages/react/vitest.config.ts
+++ packages/react/vitest.config.ts
@@ -1,10 +1,4 @@
 import { forPackage } from '@suspensive/vitest'
 import { defineConfig } from 'vitest/config'
 
-export default defineConfig(
-  forPackage({
-    test: {
-      name: 'suspensive renamed test name',
-    },
-  })
-)
+export default defineConfig(forPackage())
