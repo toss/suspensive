d4c4b71c2bf11fa6b9787d7c1455297686fb71a9
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-09-06 00:41:54 +0900

    feat(vitest): add @suspensive/vitest generating named config automatically by packageJson.name (#131)
    * feat(vitest): add @suspensive/vitest generating named config automatically by packageJson.name
    
    * Update packages/react/vitest.config.ts
    
    Co-authored-by: 박찬혁 <pgg6713@gmail.com>
    
    * ci(.github/workflows): update
    
    * Create dry-phones-applaud.md
    
    ---------
    
    Co-authored-by: 박찬혁 <pgg6713@gmail.com>

---
+++ configs/vitest/src/index.ts
@@ -0,0 +1,25 @@
+import fs from 'fs'
+import path, { dirname } from 'path'
+import { UserConfig } from 'vitest/config'
+
+export const forPackage = (userConfig?: UserConfig): UserConfig => {
+  const packageJsonPath = path.resolve(dirname('.'), 'package.json')
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'))
+
+  return {
+    ...userConfig,
+    test: {
+      name: packageJson.name,
+      dir: './src',
+      watch: false,
+      environment: 'jsdom',
+      globals: true,
+      setupFiles: './test.setup.ts',
+      coverage: {
+        provider: 'v8',
+        ...userConfig?.test?.coverage,
+      },
+      ...userConfig?.test,
+    },
+  }
+}
