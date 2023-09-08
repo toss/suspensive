a9ba1f27254e2ebea34aaeb2f423c3056eed4812
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-09-09 00:52:34 +0900

    Update websites/visualization/src/app/CommonLayout.tsx
    Co-authored-by: 박찬혁 <pgg6713@gmail.com>

--- websites/visualization/src/app/CommonLayout.tsx
+++ websites/visualization/src/app/CommonLayout.tsx
@@ -10,7 +10,7 @@ export const CommonLayout = ({ children }: PropsWithChildren) => {
   return (
     <>
       <TopNavigation />
-      <Link href="/react/await">🔗 Experimental Feature: Await</Link>
+      <Link href="/react/experimental/await">🔗 Experimental Feature: Await</Link>
       <Flex.Center flex={1}>{children}</Flex.Center>
     </>
   )
