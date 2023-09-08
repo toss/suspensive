c885c4a7c921dcd2d31d599a46841c67838f565c
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-09-09 00:22:39 +0900

    feat(react): change experimental Delay, SuspensiveProvider to stable (#123)
    * fix(react): change experimental Delay, SuspensiveProvider to stable
    
    * Create chilled-rats-grab.md
    
    * test(react): update test code for SuspensiveProvider
    
    * fix(react): remove unnecessary isDelayed check
    
    * docs(react): updated
    
    * Create red-bags-battle.md
    
    * Revert "Create chilled-rats-grab.md"
    
    This reverts commit 976c3ae6accaad19ecb44280cc957ebdad9de44f.
    
    * fix: update this later

--- packages/react/src/Delay.tsx
+++ packages/react/src/Delay.tsx
@@ -1,20 +1,20 @@
-import { ReactNode, createContext, useContext, useEffect, useState } from 'react'
+import { PropsWithChildren, createContext, useContext, useEffect, useState } from 'react'
+import { PropsWithoutChildren } from './types'
 
-export const DelayContext = createContext<{ ms?: number }>({ ms: 0 })
-
-type DelayProps = { ms?: number; children: ReactNode }
-/**
- * @experimental This is experimental feature.
- */
+type DelayProps = PropsWithChildren<{
+  ms?: number
+}>
 export const Delay = ({ ms, children }: DelayProps) => {
   const delayContextValue = useContext(DelayContext)
   const delayMs = ms ?? delayContextValue.ms ?? 0
   const [isDelayed, setIsDelayed] = useState(delayMs === 0)
 
   useEffect(() => {
-    const timerId = setTimeout(() => !isDelayed && setIsDelayed(true), delayMs)
+    const timerId = setTimeout(() => setIsDelayed(true), delayMs)
     return () => clearTimeout(timerId)
   }, [delayMs])
 
   return <>{isDelayed ? children : null}</>
 }
+
+export const DelayContext = createContext<PropsWithoutChildren<DelayProps>>({ ms: 0 })
