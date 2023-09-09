9ca237395ccad84f489a97b41808079c54d53796
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-09-10 03:15:23 +0900

    fix(tsup): shortly by following globby syntax

--- configs/tsup/src/index.ts
+++ configs/tsup/src/index.ts
@@ -3,12 +3,7 @@ import { Options } from 'tsup'
 export const options: Options = {
   banner: { js: '"use client"' },
   format: ['cjs', 'esm'],
-  entry: [
-    'src/*.{ts,tsx}',
-    '!src/*.{spec,test,test-d}.*',
-    'src/experimental/*.{ts,tsx}',
-    '!src/experimental/*.{spec,test,test-d}.*',
-  ],
+  entry: ['{src,src/experimental}/*.{ts,tsx}', '!**/*.{spec,test,test-d}.*'],
   sourcemap: true,
   dts: true,
   splitting: false,
