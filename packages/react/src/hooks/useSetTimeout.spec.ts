24533446f4c7047f272c168a016945651d0571ec
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-09-19 20:35:16 +0900

    refactor(react): add useSetTimeout

---
+++ packages/react/src/hooks/useSetTimeout.spec.ts
@@ -0,0 +1,22 @@
+import { act, renderHook } from '@testing-library/react'
+import { describe, expect, it, vi } from 'vitest'
+import { MS_100 } from '../utils/toTest'
+import { useSetTimeout } from '.'
+
+vi.useFakeTimers()
+
+describe('useSetTimeout()', () => {
+  it('should run given function once after given timeout', () => {
+    const fn = vi.fn()
+    const rendered = renderHook(() => useSetTimeout(fn, MS_100))
+    expect(fn).toHaveBeenCalledTimes(0)
+    act(() => vi.advanceTimersByTime(MS_100))
+    expect(fn).toHaveBeenCalledTimes(1)
+    act(() => vi.advanceTimersByTime(MS_100))
+    expect(fn).toHaveBeenCalledTimes(1)
+    rendered.rerender()
+    expect(fn).toHaveBeenCalledTimes(1)
+    act(() => vi.advanceTimersByTime(MS_100))
+    expect(fn).toHaveBeenCalledTimes(1)
+  })
+})
