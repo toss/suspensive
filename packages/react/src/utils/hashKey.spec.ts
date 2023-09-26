5d4a5701fddebf5d6da6611404151222592f5908
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-09-27 01:01:13 +0900

    refactor(react): divide isPlainObject as a file

--- packages/react/src/utils/hashKey.spec.ts
+++ packages/react/src/utils/hashKey.spec.ts
@@ -1,4 +1,4 @@
-import { hashKey, isPlainObject } from './hashKey'
+import { hashKey } from './hashKey'
 
 const key1 = [
   {
@@ -22,48 +22,5 @@ describe('JSON.stringify', () => {
 describe('hashKey', () => {
   it("should make same string regardless of key's field order", () => {
     expect(hashKey(key1) === hashKey(key2)).toBe(true)
-  })
-})
-
-describe('isPlainObject', () => {
-  it('should return true for a plain object', () => {
-    expect(isPlainObject({})).toBe(true)
-  })
-
-  it('should return true for an object without a constructor', () => {
-    expect(isPlainObject(Object.create(null))).toBe(true)
-  })
-
-  it('should return false for an array', () => {
-    expect(isPlainObject([])).toBe(false)
-  })
-
-  it('should return false for a null value', () => {
-    expect(isPlainObject(null)).toBe(false)
-  })
-
-  it('should return false for an undefined value', () => {
-    expect(isPlainObject(undefined)).toBe(false)
-  })
-
-  it('should return false for an object instance without an Object-specific method', () => {
-    class Foo {
-      abc: any
-      constructor() {
-        this.abc = {}
-      }
-    }
-    expect(isPlainObject(new Foo())).toBe(false)
-  })
-
-  it('should return false for an object with a custom prototype', () => {
-    function Graph(this: any) {
-      this.vertices = []
-      this.edges = []
-    }
-    Graph.prototype.addVertex = function (v: any) {
-      this.vertices.push(v)
-    }
-    expect(isPlainObject(Object.create(Graph))).toBe(false)
   })
 })
