0e67e786c55756a6fd5a5da840c5d0a679771b75
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-10-11 21:23:52 +0900

    refactor(react): to easier code (#217)
    * refactor(react): to easier code
    
    * chore: update
    
    * test(react): update
    
    * Create olive-hounds-nail.md

--- packages/react/src/utils/assert.ts
+++ packages/react/src/utils/assert.ts
@@ -3,3 +3,13 @@ export function assert(condition: boolean, message: string): asserts condition {
     throw new Error(message)
   }
 }
+
+assert.message = {
+  useErrorBoundary: {
+    onlyInChildrenOfErrorBoundary: 'useErrorBoundary: this hook should be called in ErrorBoundary.props.children',
+  },
+  useErrorBoundaryGroup: {
+    onlyInChildrenOfErrorBoundaryGroup:
+      'useErrorBoundaryGroup: this hook should be called in ErrorBoundaryGroup.props.children',
+  },
+} as const
