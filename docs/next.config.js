0942e8bdaacc005e6c60d876ef9309c127bc23f5
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-10-17 01:49:45 +0900

    docs(docs): init new official docs using nextra (#230)
    Co-authored-by: minsoo kim <zlemzlem5656@naver.com>
    Co-authored-by: Chanhyuk Park <pgg6713@gmail.com>

---
+++ docs/next.config.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+})
+
+module.exports = withNextra({
+  i18n: {
+    locales: ['en', 'ko'],
+    defaultLocale: 'en',
+  },
+})
---
+++ docs/src/middleware.ts
@@ -0,0 +1,5 @@
+import { withLocales } from 'nextra/locales'
+
+export const middleware = withLocales(() => {
+  // Your middleware code...
+})
