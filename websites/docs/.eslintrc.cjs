ff83c421b6b96ea4714f2dbf5cbd2d19bbbdc315
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-10-18 22:01:07 +0900

    chore: remove docs in packages (#232)
    * chore: remove docs in packages
    
    * chore: update
    
    * chore: update
    
    * Create four-roses-pay.md

--- websites/docs/.eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ['@suspensive/eslint-config/react-ts'],
-  rules: {
-    // @theme, @docusaurus, etc aren't actual paths. They are webpack aliases. To prevent ESLint from tripping up on those, you could ignore those in the ESLint settings
-    'import/no-unresolved': [2, { ignore: ['^@theme', '^@docusaurus', '^@site'] }],
-  },
-}
