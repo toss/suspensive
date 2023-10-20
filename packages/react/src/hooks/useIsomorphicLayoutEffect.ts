0336ae31fa338e15f8ef111c39a14e6683fd4034
김민수 <57122180+minsoo-web@users.noreply.github.com>
2023-10-20 16:48:27 +0900

    Update packages/react/src/hooks/useIsomorphicLayoutEffect.ts
    Co-authored-by: Jonghyeon Ko <manudeli.ko@gmail.com>

--- packages/react/src/hooks/useIsomorphicLayoutEffect.ts
+++ packages/react/src/hooks/useIsomorphicLayoutEffect.ts
@@ -1,6 +1,3 @@
 import { useEffect, useLayoutEffect } from 'react'
-import { isClient } from '../utils/isClient'
-
-const isClientEnv = isClient()
-
-export const useIsomorphicLayoutEffect = isClientEnv ? useLayoutEffect : useEffect
+const isClient = typeof window !== 'undefined'
+export const useIsomorphicLayoutEffect = isClient ? useLayoutEffect : useEffect
