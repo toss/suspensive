79fecd9a87a63e71cffd321d0e0e22208384e1c5
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-10-26 18:45:18 +0900

    perf(react): remove unnecessary useSetTimeout's callback calling
    Co-authored-by: Gromit (전민재) <ssi02014@naver.com>

--- packages/react/src/hooks/useTimeout.spec.ts
+++ packages/react/src/hooks/useTimeout.spec.ts
@@ -1,14 +1,14 @@
 import { MS_100 } from '@suspensive/test-utils'
 import { act, renderHook } from '@testing-library/react'
 import { describe, expect, it, vi } from 'vitest'
-import { useSetTimeout } from '.'
+import { useTimeout } from '.'
 
 vi.useFakeTimers()
 
-describe('useSetTimeout', () => {
+describe('useTimeout', () => {
   it('should run given function once after given timeout', () => {
     const fn = vi.fn()
-    const rendered = renderHook(() => useSetTimeout(fn, MS_100))
+    const rendered = renderHook(() => useTimeout(fn, MS_100))
     expect(fn).toHaveBeenCalledTimes(0)
     act(() => vi.advanceTimersByTime(MS_100))
     expect(fn).toHaveBeenCalledTimes(1)
