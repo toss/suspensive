b962122f0ce704921d6c27eac22e0e25eae4dc28
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2023-10-28 01:12:45 +0900

    test(react): add useTimeout test case (#268)
    fix #265 
    
    # Overview
    @manudeli 👏
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/suspensive/react/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.
    
    ---------
    
    Co-authored-by: Jonghyeon Ko <jonghyeon.ko@wesang.com>

---
+++ packages/react/src/hooks/useTimeout.spec.tsx
@@ -0,0 +1,47 @@
+import { MS_100 } from '@suspensive/test-utils'
+import { act, render, renderHook, screen } from '@testing-library/react'
+import { useState } from 'react'
+import { describe, expect, it, vi } from 'vitest'
+import { useTimeout } from '.'
+
+vi.useFakeTimers()
+
+const TestComponent = () => {
+  const [number, setNumber] = useState(0)
+
+  useTimeout(() => {
+    setNumber(number + 1)
+  }, MS_100)
+
+  return <div>{number}</div>
+}
+
+describe('useTimeout', () => {
+  it('should run given function once after given timeout', () => {
+    const fn = vi.fn()
+    const rendered = renderHook(() => useTimeout(fn, MS_100))
+    expect(fn).toHaveBeenCalledTimes(0)
+    act(() => vi.advanceTimersByTime(MS_100))
+    expect(fn).toHaveBeenCalledTimes(1)
+    act(() => vi.advanceTimersByTime(MS_100))
+    expect(fn).toHaveBeenCalledTimes(1)
+    rendered.rerender()
+    expect(fn).toHaveBeenCalledTimes(1)
+    act(() => vi.advanceTimersByTime(MS_100))
+    expect(fn).toHaveBeenCalledTimes(1)
+  })
+
+  it('should not re-call callback reveived as argument even if component using this hook is rerendered', () => {
+    const { rerender } = render(<TestComponent />)
+
+    expect(screen.getByText('0')).toBeInTheDocument()
+
+    act(() => vi.advanceTimersByTime(MS_100))
+    expect(screen.getByText('1')).toBeInTheDocument()
+
+    rerender(<TestComponent />)
+
+    act(() => vi.advanceTimersByTime(MS_100))
+    expect(screen.getByText('1')).toBeInTheDocument()
+  })
+})
