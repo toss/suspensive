07eb38da8054bbe26a05aa543d64dcb6bd5379ad
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-10-27 23:07:40 +0900

    perf(react): remove unnecessary useSetTimeout's callback calling (#264)
    fix #263 
    
    # Overview
    
    <!--
        A clear and concise description of what this pr is about.
     -->
    
    @ssi02014 Thanks for reporting important issue. Could you review this
    Pull Request too?
    I remove unnecessary useSetTimeout's callback calling and I added you as
    co-author in
    [79fecd9](https://github.com/suspensive/react/pull/264/commits/79fecd9a87a63e71cffd321d0e0e22208384e1c5)
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/suspensive/react/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.
    
    ---------
    
    Co-authored-by: Gromit (전민재) <ssi02014@naver.com>

--- packages/react/src/hooks/useSetTimeout.ts
+++
@@ -1,7 +0,0 @@
-import { useEffect } from 'react'
-
-export const useSetTimeout = (fn: (...args: []) => void, delay: number) =>
-  useEffect(() => {
-    const timeout = setTimeout(fn, delay)
-    return () => clearTimeout(timeout)
-  }, [fn, delay])
