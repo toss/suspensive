35f9d21606696ab0a223783d1fdcce125a72ef28
ssi02014 <ssi02014@naver.com>
2023-10-28 00:45:50 +0900

    test(react): add useTimeout Test Code

--- packages/react/src/hooks/useTimeout.spec.tsx
+++ packages/react/src/hooks/useTimeout.spec.tsx
@@ -1,9 +1,20 @@
 import { MS_100 } from '@suspensive/test-utils'
-import { act, renderHook } from '@testing-library/react'
+import { act, render, renderHook, screen } from '@testing-library/react'
+import { useState } from 'react'
 import { describe, expect, it, vi } from 'vitest'
 import { useTimeout } from '.'
 
 vi.useFakeTimers()
+
+const TestComponent = () => {
+  const [number, setNumber] = useState(0)
+
+  useTimeout(() => {
+    setNumber(number + 1)
+  }, MS_100)
+
+  return <div>{number}</div>
+}
 
 describe('useTimeout', () => {
   it('should run given function once after given timeout', () => {
@@ -18,5 +29,19 @@ describe('useTimeout', () => {
     expect(fn).toHaveBeenCalledTimes(1)
     act(() => vi.advanceTimersByTime(MS_100))
     expect(fn).toHaveBeenCalledTimes(1)
+  })
+
+  it('should not be re-called even if the component is re-rendered', () => {
+    const { rerender } = render(<TestComponent />)
+
+    expect(screen.getByText('0')).toBeInTheDocument()
+
+    act(() => vi.advanceTimersByTime(MS_100))
+    expect(screen.getByText('1')).toBeInTheDocument()
+
+    rerender(<TestComponent />)
+
+    act(() => vi.advanceTimersByTime(MS_100))
+    expect(screen.getByText('1')).toBeInTheDocument()
   })
 })
