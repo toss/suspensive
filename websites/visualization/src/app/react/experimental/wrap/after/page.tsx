5e85da05bbf6452fbc515e0d3b198efde26d840d
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-10-28 18:51:34 +0900

    chore: update

--- websites/visualization/src/app/react/experimental/wrap/after/page.tsx
+++ websites/visualization/src/app/react/experimental/wrap/after/page.tsx
@@ -1,28 +1,23 @@
 'use client'
-import { useErrorBoundary, withErrorBoundary, withErrorBoundaryGroup, withSuspense } from '@suspensive/react'
+import { ErrorBoundary, ErrorBoundaryGroup, Suspense, useErrorBoundary, wrap } from '@suspensive/react'
 import { UseSuspenseQuery } from '~/components'
 import { api } from '~/utils'
 
 const logError = (error: Error) => console.error(error)
 
-export default withErrorBoundaryGroup(
-  withErrorBoundary(
-    withSuspense(
-      () => {
-        const errorBoundary = useErrorBoundary()
+export default wrap(ErrorBoundaryGroup, { blockOutside: false })(
+  wrap(ErrorBoundary, { fallback: (props) => <>{props.error.message}</>, onError: logError })(
+    wrap(Suspense.CSROnly, { fallback: <>loading...</> })(() => {
+      const errorBoundary = useErrorBoundary()
 
-        return (
-          <>
-            <button onClick={() => errorBoundary.setError(new Error('trigger error by useErrorBoundary().setError'))}>
-              trigger error by useErrorBoundary().setError
-            </button>
-            <UseSuspenseQuery queryKey={['wrap', 1] as const} queryFn={() => api.delay(200, { percentage: 50 })} />
-          </>
-        )
-      },
-      { fallback: <>loading...</> }
-    ),
-    { fallback: (props) => <>{props.error.message}</>, onError: logError }
-  ),
-  { blockOutside: false }
+      return (
+        <>
+          <button onClick={() => errorBoundary.setError(new Error('trigger error by useErrorBoundary().setError'))}>
+            trigger error by useErrorBoundary().setError
+          </button>
+          <UseSuspenseQuery queryKey={['with', 1] as const} queryFn={() => api.delay(200, { percentage: 50 })} />
+        </>
+      )
+    })
+  )
 )
--- websites/visualization/src/app/react/experimental/wrap/before/page.tsx
+++ websites/visualization/src/app/react/experimental/wrap/before/page.tsx
@@ -1,23 +1,28 @@
 'use client'
-import { ErrorBoundary, ErrorBoundaryGroup, Suspense, useErrorBoundary, wrap } from '@suspensive/react'
+import { useErrorBoundary, withErrorBoundary, withErrorBoundaryGroup, withSuspense } from '@suspensive/react'
 import { UseSuspenseQuery } from '~/components'
 import { api } from '~/utils'
 
 const logError = (error: Error) => console.error(error)
 
-export default wrap(ErrorBoundaryGroup, { blockOutside: false })(
-  wrap(ErrorBoundary, { fallback: (props) => <>{props.error.message}</>, onError: logError })(
-    wrap(Suspense, { fallback: <>loading...</> })(() => {
-      const errorBoundary = useErrorBoundary()
+export default withErrorBoundaryGroup(
+  withErrorBoundary(
+    withSuspense.CSROnly(
+      () => {
+        const errorBoundary = useErrorBoundary()
 
-      return (
-        <>
-          <button onClick={() => errorBoundary.setError(new Error('trigger error by useErrorBoundary().setError'))}>
-            trigger error by useErrorBoundary().setError
-          </button>
-          <UseSuspenseQuery queryKey={['with', 1] as const} queryFn={() => api.delay(200, { percentage: 50 })} />
-        </>
-      )
-    })
-  )
+        return (
+          <>
+            <button onClick={() => errorBoundary.setError(new Error('trigger error by useErrorBoundary().setError'))}>
+              trigger error by useErrorBoundary().setError
+            </button>
+            <UseSuspenseQuery queryKey={['wrap', 1] as const} queryFn={() => api.delay(200, { percentage: 50 })} />
+          </>
+        )
+      },
+      { fallback: <>loading...</> }
+    ),
+    { fallback: (props) => <>{props.error.message}</>, onError: logError }
+  ),
+  { blockOutside: false }
 )
