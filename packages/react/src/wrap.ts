205f7567eefa46fa378aa2067af179f3e8e4bc8a
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-10-31 22:00:58 +0900

    refactor(react): clarify wrap

--- packages/react/src/wrap.ts
+++ packages/react/src/wrap.ts
@@ -1,105 +1,110 @@
-import { type ComponentProps, type ComponentType, createElement } from 'react'
+import { createElement } from 'react'
+import type { ComponentProps, ComponentType } from 'react'
 import type { PropsWithoutChildren } from './types'
 import { AsyncBoundary, Delay, ErrorBoundary, ErrorBoundaryGroup, Suspense } from '.'
-import type { AsyncBoundaryProps, DelayProps, ErrorBoundaryGroupProps, ErrorBoundaryProps, SuspenseProps } from '.'
+
+type WrapperItem<
+  TWrapperComponent extends
+    | typeof Suspense
+    | typeof Suspense.CSROnly
+    | typeof ErrorBoundary
+    | typeof ErrorBoundaryGroup
+    | typeof AsyncBoundary
+    | typeof AsyncBoundary.CSROnly
+    | typeof Delay
+> = [TWrapperComponent, PropsWithoutChildren<ComponentProps<TWrapperComponent>>]
 
 type Wrapper =
-  | { component: typeof Suspense; props: PropsWithoutChildren<SuspenseProps> }
-  | { component: typeof Suspense.CSROnly; props: PropsWithoutChildren<SuspenseProps> }
-  | { component: typeof ErrorBoundary; props: PropsWithoutChildren<ErrorBoundaryProps> }
-  | { component: typeof ErrorBoundaryGroup; props: PropsWithoutChildren<ErrorBoundaryGroupProps> }
-  | { component: typeof AsyncBoundary; props: PropsWithoutChildren<AsyncBoundaryProps> }
-  | { component: typeof AsyncBoundary.CSROnly; props: PropsWithoutChildren<AsyncBoundaryProps> }
-  | { component: typeof Delay; props: PropsWithoutChildren<DelayProps> }
+  | WrapperItem<typeof Suspense>
+  | WrapperItem<typeof Suspense.CSROnly>
+  | WrapperItem<typeof ErrorBoundary>
+  | WrapperItem<typeof ErrorBoundaryGroup>
+  | WrapperItem<typeof AsyncBoundary>
+  | WrapperItem<typeof AsyncBoundary.CSROnly>
+  | WrapperItem<typeof Delay>
 
-class WrapBuilder {
+class WrapWithoutCSROnly {
   constructor(private wrappers: Wrapper[]) {}
-  Suspense = (props: PropsWithoutChildren<SuspenseProps> = {}) => {
-    this.wrappers.unshift({ component: Suspense, props })
+  Suspense = (props: PropsWithoutChildren<ComponentProps<typeof Suspense>> = {}) => {
+    this.wrappers.unshift([Suspense, props])
     return this
   }
-  ErrorBoundary = (props: PropsWithoutChildren<ErrorBoundaryProps>) => {
-    this.wrappers.unshift({ component: ErrorBoundary, props })
+  ErrorBoundary = (props: PropsWithoutChildren<ComponentProps<typeof ErrorBoundary>>) => {
+    this.wrappers.unshift([ErrorBoundary, props])
     return this
   }
-  ErrorBoundaryGroup = (props: PropsWithoutChildren<ErrorBoundaryGroupProps> = {}) => {
-    this.wrappers.unshift({ component: ErrorBoundaryGroup, props })
+  ErrorBoundaryGroup = (props: PropsWithoutChildren<ComponentProps<typeof ErrorBoundaryGroup>> = {}) => {
+    this.wrappers.unshift([ErrorBoundaryGroup, props])
     return this
   }
-  AsyncBoundary = (props: PropsWithoutChildren<AsyncBoundaryProps>) => {
-    this.wrappers.unshift({ component: AsyncBoundary, props })
+  AsyncBoundary = (props: PropsWithoutChildren<ComponentProps<typeof AsyncBoundary>>) => {
+    this.wrappers.unshift([AsyncBoundary, props])
     return this
   }
-  Delay = (props: PropsWithoutChildren<DelayProps> = {}) => {
-    this.wrappers.unshift({ component: Delay, props })
+  Delay = (props: PropsWithoutChildren<ComponentProps<typeof Delay>> = {}) => {
+    this.wrappers.unshift([Delay, props])
     return this
   }
 
   on = <TProps extends ComponentProps<ComponentType>>(component: ComponentType<TProps>) => {
-    const wrapped = (props: TProps) =>
+    const wrappedComponent = (props: TProps) =>
       this.wrappers.reduce(
-        (acc, wrapper) => createElement(wrapper.component as any, wrapper.props as any, acc),
+        (acc, [wrapperComponent, wrapperProps]) => createElement(wrapperComponent as any, wrapperProps as any, acc),
         createElement(component, props)
       )
 
     if (process.env.NODE_ENV !== 'production') {
-      wrapped.displayName = this.wrappers.reduce(
-        (acc, wrapper) => `with${wrapper.component.displayName}(${acc})`,
+      wrappedComponent.displayName = this.wrappers.reduce(
+        (acc, [wrapperComponent]) => `with${wrapperComponent.displayName}(${acc})`,
         component.displayName || component.name || 'Component'
       )
     }
 
-    return wrapped
+    return wrappedComponent
   }
 }
 
-type WrapBuilderWithCSROnly = WrapBuilder & {
-  Suspense: WrapBuilder['Suspense'] & {
-    CSROnly: (props?: PropsWithoutChildren<SuspenseProps>) => WrapBuilder
+type Wrap = WrapWithoutCSROnly & {
+  Suspense: WrapWithoutCSROnly['Suspense'] & {
+    CSROnly: (props?: PropsWithoutChildren<ComponentProps<typeof Suspense.CSROnly>>) => Wrap
   }
-  AsyncBoundary: WrapBuilder['AsyncBoundary'] & {
-    CSROnly: (props: PropsWithoutChildren<AsyncBoundaryProps>) => WrapBuilder
+  AsyncBoundary: WrapWithoutCSROnly['AsyncBoundary'] & {
+    CSROnly: (props: PropsWithoutChildren<ComponentProps<typeof AsyncBoundary.CSROnly>>) => Wrap
   }
 }
 
-const injectCSROnly = (builder: WrapBuilderWithCSROnly, wrappers: Wrapper[]) => {
-  builder.Suspense.CSROnly = (props: PropsWithoutChildren<SuspenseProps> = {}) => {
-    wrappers.unshift({ component: Suspense.CSROnly, props })
+const createWrap = () => {
+  const wrappers: Wrapper[] = []
+  const builder = new WrapWithoutCSROnly(wrappers) as Wrap
+  builder.Suspense.CSROnly = (props: PropsWithoutChildren<ComponentProps<typeof Suspense.CSROnly>> = {}) => {
+    wrappers.unshift([Suspense.CSROnly, props])
     return builder
   }
-  builder.AsyncBoundary.CSROnly = (props: PropsWithoutChildren<AsyncBoundaryProps>) => {
-    wrappers.unshift({ component: AsyncBoundary.CSROnly, props })
+  builder.AsyncBoundary.CSROnly = (props: PropsWithoutChildren<ComponentProps<typeof AsyncBoundary.CSROnly>>) => {
+    wrappers.unshift([AsyncBoundary.CSROnly, props])
     return builder
   }
-}
-
-const createBuilder = (initialWrapper: Wrapper) => {
-  const wrappers: Wrapper[] = [initialWrapper]
-  const builder = new WrapBuilder(wrappers) as WrapBuilderWithCSROnly
-  injectCSROnly(builder, wrappers)
   return builder
 }
 
-const buildSuspense = (props: PropsWithoutChildren<SuspenseProps> = {}) => createBuilder({ component: Suspense, props })
-buildSuspense.CSROnly = (props: PropsWithoutChildren<SuspenseProps> = {}) =>
-  createBuilder({ component: Suspense.CSROnly, props })
-const buildErrorBoundary = (props: PropsWithoutChildren<ErrorBoundaryProps>) =>
-  createBuilder({ component: ErrorBoundary, props })
-const buildErrorBoundaryGroup = (props: PropsWithoutChildren<ErrorBoundaryGroupProps> = {}) =>
-  createBuilder({ component: ErrorBoundaryGroup, props })
-const buildAsyncBoundary = (props: PropsWithoutChildren<AsyncBoundaryProps>) =>
-  createBuilder({ component: AsyncBoundary, props })
-buildAsyncBoundary.CSROnly = (props: PropsWithoutChildren<AsyncBoundaryProps>) =>
-  createBuilder({ component: AsyncBoundary.CSROnly, props })
-const buildDelay = (props: PropsWithoutChildren<DelayProps> = {}) => createBuilder({ component: Delay, props })
+const wrapSuspense = (...[props = {}]: Parameters<Wrap['Suspense']>) => createWrap().Suspense(props)
+wrapSuspense.CSROnly = (...[props = {}]: Parameters<Wrap['Suspense']['CSROnly']>) =>
+  createWrap().Suspense.CSROnly(props)
+const wrapErrorBoundary = (...[props]: Parameters<Wrap['ErrorBoundary']>) => createWrap().ErrorBoundary(props)
+const wrapErrorBoundaryGroup = (...[props = {}]: Parameters<Wrap['ErrorBoundaryGroup']>) =>
+  createWrap().ErrorBoundaryGroup(props)
+const wrapAsyncBoundary = (...[props]: Parameters<Wrap['AsyncBoundary']>) => createWrap().AsyncBoundary(props)
+wrapAsyncBoundary.CSROnly = (...[props]: Parameters<Wrap['AsyncBoundary']['CSROnly']>) =>
+  createWrap().AsyncBoundary.CSROnly(props)
+const wrapDelay = (...[props = {}]: Parameters<Wrap['Delay']>) => createWrap().Delay(props)
 
 /**
  * @experimental This is experimental feature.
  */
 export const wrap = {
-  Suspense: buildSuspense,
-  ErrorBoundary: buildErrorBoundary,
-  ErrorBoundaryGroup: buildErrorBoundaryGroup,
-  AsyncBoundary: buildAsyncBoundary,
-  Delay: buildDelay,
+  Suspense: wrapSuspense,
+  ErrorBoundary: wrapErrorBoundary,
+  ErrorBoundaryGroup: wrapErrorBoundaryGroup,
+  AsyncBoundary: wrapAsyncBoundary,
+  Delay: wrapDelay,
 }
