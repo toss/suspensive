79a2c50f7984081c35d897cc07e9fa155a5bba0c
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-11-08 01:39:22 +0900

    chore: eslint to latest (#304)
    # Overview
    
    <!--
        A clear and concise description of what this pr is about.
     -->
    
    eslint to latest
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/suspensive/react/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.

--- configs/eslint-config-ts/noimport.js
+++ configs/eslint-config-ts/noimport.js
@@ -9,9 +9,9 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
   ],
   rules: {
-    '@typescript-eslint/no-empty-function': ['off'],
     '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
     'jsdoc/require-description': 'warn',
     'jsdoc/require-returns': 'off',
     'jsdoc/require-jsdoc': 'off',
