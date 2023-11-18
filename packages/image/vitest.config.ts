1ddee436372f5b9f605e92514cf85f1954690043
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-11-19 01:05:18 +0900

    chore: update

--- packages/image/vitest.config.ts
+++ packages/image/vitest.config.ts
@@ -1,12 +1,13 @@
-import { getPackageJsonName } from '@suspensive/package-json-name'
+import packageJsonName from '@suspensive/package-json-name'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    name: getPackageJsonName(),
+    name: packageJsonName(),
     dir: './src',
     environment: 'jsdom',
     globals: true,
+    setupFiles: './vitest.setup.ts',
     coverage: {
       provider: 'istanbul',
     },
