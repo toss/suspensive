29e300329ea0394070a12a2ba6e072e7258ab7ed
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-11-27 07:10:41 +0900

    feat(react-await): add GarbageCollectable

---
+++ packages/react-await/src/GarbageCollectable.ts
@@ -0,0 +1,36 @@
+function isValidTimeout(value: unknown): value is number {
+  return typeof value === 'number' && value >= 0 && value !== Infinity
+}
+
+export abstract class GarbageCollectable {
+  gcTime!: number
+  gcTimeout?: ReturnType<typeof setTimeout>
+
+  destroy(): void {
+    this.clearGcTimeout()
+  }
+
+  protected scheduleGc(): void {
+    this.clearGcTimeout()
+
+    if (isValidTimeout(this.gcTime)) {
+      this.gcTimeout = setTimeout(() => {
+        this.optionalRemove()
+      }, this.gcTime)
+    }
+  }
+
+  protected updateGcTime(newGcTime: number | undefined): void {
+    // Default to 5 minutes (Infinity for server-side) if no gcTime is set
+    this.gcTime = Math.max(this.gcTime || 0, newGcTime ?? 5 * 60 * 1000)
+  }
+
+  protected clearGcTimeout() {
+    if (this.gcTimeout) {
+      clearTimeout(this.gcTimeout)
+      this.gcTimeout = undefined
+    }
+  }
+
+  protected abstract optionalRemove(): void
+}
