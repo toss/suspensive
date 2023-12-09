39ca18a9ca4835fa5ec672c4488c138ebcec283d
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-12-09 10:43:16 +0900

    chore(visualization): replace emotion to tailwindcss (#448)
    # Overview
    
    <!--
        A clear and concise description of what this pr is about.
     -->
    
    Remove emotion, jsxcss
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/suspensive/react/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.

--- websites/visualization/src/app/page.tsx
+++ websites/visualization/src/app/page.tsx
@@ -1,23 +1,19 @@
-/** @jsxImportSource @emotion/react */
-'use client'
-
-import { Flex } from '@jsxcss/emotion'
 import Link from 'next/link'
 import { Area, Box } from '~/components/uis'
 
 export default function Home() {
   return (
-    <Flex flex={1} justify={'center'} margin={'-5vh 16px 0 16px'}>
+    <div className="-mt-5 ml-4 mr-4 flex flex-1 justify-center">
       <Area title="Check Concepts">
-        <Flex justify={'space-between'} gap={16} direction={['column', 'row']}>
-          <Link href="/react" style={{ flex: 1 }}>
+        <div className="flex flex-col justify-between gap-4 md:flex-row">
+          <Link href="/react" className="flex-1">
             <Box.Default>🔗 @suspensive/react</Box.Default>
           </Link>
-          <Link href="/react-query" style={{ flex: 1 }}>
+          <Link href="/react-query" className="flex-1">
             <Box.Default>🔗 @suspensive/react-query</Box.Default>
           </Link>
-        </Flex>
+        </div>
       </Area>
-    </Flex>
+    </div>
   )
 }
