6373ee93d747a5cfd424d875558566200face656
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-12-12 01:13:30 +0900

    chore: remove toDiff

--- toDiff/PropsWithoutChildren-l3xNTJvW.d.ts
+++
@@ -1,5 +0,0 @@
-import { ComponentProps, ComponentType } from 'react'
-
-type PropsWithoutChildren<TProps extends ComponentProps<ComponentType>> = Omit<TProps, 'children'>
-
-export type { PropsWithoutChildren as P }
