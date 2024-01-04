90e6643c7d509062317820b821c98dbd0938a234
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-01-05 00:53:39 +0900

    feat(react): add fallback prop of Delay

--- packages/react/src/Delay.tsx
+++ packages/react/src/Delay.tsx
@@ -1,17 +1,18 @@
-import { type PropsWithChildren, useContext, useState } from 'react'
+import { type PropsWithChildren, type ReactNode, useContext, useState } from 'react'
 import { DelayDefaultPropsContext } from './contexts'
 import { useTimeout } from './hooks'
 
 export interface DelayProps extends PropsWithChildren {
   ms?: number
+  fallback?: ReactNode
 }
 
-export const Delay = ({ ms, children }: DelayProps) => {
+export const Delay = ({ ms, children, fallback }: DelayProps) => {
   const defaultProps = useContext(DelayDefaultPropsContext)
   const delayMs = ms ?? defaultProps.ms ?? 0
   const [isDelayed, setIsDelayed] = useState(delayMs === 0)
   useTimeout(() => setIsDelayed(true), delayMs)
-  return <>{isDelayed ? children : null}</>
+  return <>{isDelayed ? children : typeof fallback === 'undefined' ? defaultProps.fallback : fallback}</>
 }
 if (process.env.NODE_ENV !== 'production') {
   Delay.displayName = 'Delay'
