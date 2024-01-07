51e6a36abb5ce5ff49846f26656086ab86957cd7
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-01-08 02:46:06 +0900

    Update packages/react/src/AsyncBoundary.tsx

--- packages/react/src/AsyncBoundary.tsx
+++ packages/react/src/AsyncBoundary.tsx
@@ -20,7 +20,7 @@ export const AsyncBoundary = Object.assign(
   (() => {
     const BaseAsyncBoundary = forwardRef<ComponentRef<typeof ErrorBoundary>, AsyncBoundaryProps>(
       ({ clientOnly, pendingFallback, rejectedFallback, children, ...errorBoundaryProps }, resetRef) => (
-        <ErrorBoundary {...(errorBoundaryProps as ErrorBoundaryProps)} ref={resetRef} fallback={rejectedFallback}>
+        <ErrorBoundary {...errorBoundaryProps} ref={resetRef} fallback={rejectedFallback}>
           <Suspense clientOnly={clientOnly} fallback={pendingFallback}>
             {children}
           </Suspense>
