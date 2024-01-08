aafcfc6f0f560647a18918b0dc74d8068bece787
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-01-08 18:32:57 +0900

    fix(react): rename isDelayed as isDelaying

--- packages/react/src/Delay.tsx
+++ packages/react/src/Delay.tsx
@@ -1,6 +1,7 @@
 import { type PropsWithChildren, type ReactNode, useContext, useState } from 'react'
 import { DelayDefaultPropsContext } from './contexts'
 import { useTimeout } from './hooks'
+import { assert } from './utils'
 
 export interface DelayProps extends PropsWithChildren {
   ms?: number
@@ -13,9 +14,14 @@ export interface DelayProps extends PropsWithChildren {
 export const Delay = ({ ms, children, fallback }: DelayProps) => {
   const defaultProps = useContext(DelayDefaultPropsContext)
   const delayMs = ms ?? defaultProps.ms ?? 0
-  const [isDelayed, setIsDelayed] = useState(delayMs === 0)
-  useTimeout(() => setIsDelayed(true), delayMs)
-  return <>{isDelayed ? children : typeof fallback === 'undefined' ? defaultProps.fallback : fallback}</>
+
+  if (process.env.NODE_ENV !== 'production') {
+    assert(delayMs >= 0, 'Delay: ms prop should be greater than or equal to 0')
+  }
+
+  const [isDelaying, setIsDelaying] = useState(delayMs > 0)
+  useTimeout(() => setIsDelaying(false), delayMs)
+  return isDelaying ? (typeof fallback === 'undefined' ? defaultProps.fallback : fallback) : children
 }
 if (process.env.NODE_ENV !== 'production') {
   Delay.displayName = 'Delay'
