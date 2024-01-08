4b501867058707f4444af0b5c3f3654acf292b7d
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-01-08 18:46:18 +0900

    chore: update

--- packages/react/src/Delay.tsx
+++ packages/react/src/Delay.tsx
@@ -11,17 +11,20 @@ export interface DelayProps extends PropsWithChildren {
   fallback?: ReactNode
 }
 
-export const Delay = ({ ms, children, fallback }: DelayProps) => {
+export const Delay = (props: DelayProps) => {
   const defaultProps = useContext(DelayDefaultPropsContext)
-  const delayMs = ms ?? defaultProps.ms ?? 0
 
+  const ms = props.ms ?? defaultProps.ms ?? 0
   if (process.env.NODE_ENV !== 'production') {
-    assert(delayMs >= 0, 'Delay: ms prop should be greater than or equal to 0')
+    assert(ms >= 0, 'Delay: ms prop should be greater than or equal to 0')
   }
 
-  const [isDelaying, setIsDelaying] = useState(delayMs > 0)
-  useTimeout(() => setIsDelaying(false), delayMs)
-  return isDelaying ? (typeof fallback === 'undefined' ? defaultProps.fallback : fallback) : children
+  const [isDelaying, setIsDelaying] = useState(ms > 0)
+  useTimeout(() => setIsDelaying(false), ms)
+
+  const fallback = typeof props.fallback === 'undefined' ? defaultProps.fallback : props.fallback
+
+  return isDelaying ? fallback : props.children
 }
 if (process.env.NODE_ENV !== 'production') {
   Delay.displayName = 'Delay'
