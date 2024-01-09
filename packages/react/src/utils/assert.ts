23798e761cc91f8af8d163a7cb0126159ba667c6
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-01-10 00:34:53 +0900

    fix(react): assert message correctly, test code simply (#579)
    # Overview
    
    <!--
        A clear and concise description of what this pr is about.
     -->
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/suspensive/react/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.

--- packages/react/src/utils/assert.ts
+++ packages/react/src/utils/assert.ts
@@ -4,9 +4,11 @@ export function assert(condition: boolean, message: string): asserts condition {
   }
 }
 
-export const assertMessageUseErrorBoundaryOnlyInChildrenOfErrorBoundary =
+export const useErrorBoundary_this_hook_should_be_called_in_ErrorBoundary_props_children =
   'useErrorBoundary: this hook should be called in ErrorBoundary.props.children'
-export const assertMessageUseErrorBoundaryFallbackPropsOnlyInFallbackOfErrorBoundary =
-  'useErrorBoundary: this hook should be called in ErrorBoundary.props.children'
-export const assertMessageUseErrorBoundaryGroupOnlyInChildrenOfErrorBoundaryGroup =
-  'useErrorBoundary: this hook should be called in ErrorBoundary.props.children'
+
+export const useErrorBoundaryFallbackProps_this_hook_should_be_called_in_ErrorBoundary_props_fallback =
+  'useErrorBoundaryFallbackProps: this hook should be called in ErrorBoundary.props.fallback'
+
+export const useErrorBoundaryGroup_this_hook_should_be_called_in_ErrorBoundary_props_children =
+  'useErrorBoundaryGroup: this hook should be called in ErrorBoundary.props.children'
