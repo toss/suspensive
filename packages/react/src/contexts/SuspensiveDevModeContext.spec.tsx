efe418bb1807d82c97adb33ebb38370892fa3437
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-01-18 02:47:38 +0900

    chore: resolve eslint error

--- packages/react/src/contexts/SuspensiveDevModeContext.spec.tsx
+++ packages/react/src/contexts/SuspensiveDevModeContext.spec.tsx
@@ -34,17 +34,15 @@ describe('SuspensiveDevMode', () => {
     expect(subscriber.notifiedCount).toBe(2)
   })
 
-  describe("DevModeContext", () => {
-    it("returns null when no SuspensiveProvider is present", () => {
+  describe('DevModeContext', () => {
+    it('returns null when no SuspensiveProvider is present', () => {
       const { result } = renderHook(() => useContext(DevModeContext))
       expect(result.current).toBeNull()
     })
 
-    it("returns an instance of SuspensiveDevMode when wrapped with SuspensiveProvider", () => {
+    it('returns an instance of SuspensiveDevMode when wrapped with SuspensiveProvider', () => {
       const { result } = renderHook(() => useContext(DevModeContext), {
-        wrapper: (props) => (
-          <SuspensiveProvider {...props} value={new Suspensive()} />
-        ),
+        wrapper: (props) => <SuspensiveProvider {...props} value={new Suspensive()} />,
       })
       expect(result.current).toBeInstanceOf(SuspensiveDevMode)
     })
