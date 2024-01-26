6ad1ac2e5d98f9a63ecb2fe57a07bc1e84b88513
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-01-26 21:55:28 +0900

    fix(react): add getServerSnapshot of use-sync-external-store in for syncDevMode in SSR environment (#656)
    fix #655 
    
    # Overview
    
    <!--
        A clear and concise description of what this pr is about.
     -->
    
    @lunelson Thanks Lu Nelson we could catch this bug quickly because of
    your reporting issue.
    I added you as co-author for this Pull Request. Thanks
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/suspensive/react/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.
    
    ---------
    
    Co-authored-by: Lu Nelson <lunelson@gmail.com>

--- examples/next-streaming-react-query/src/app/components/Text2.tsx
+++ examples/next-streaming-react-query/src/app/components/Text2.tsx
@@ -1,6 +1,8 @@
 import { type ComponentProps, forwardRef } from 'react'
 
 export const Text2 = forwardRef<HTMLParagraphElement, ComponentProps<'p'>>((props, ref) => (
-  <p ref={ref}>result: {props.children}</p>
+  <p {...props} ref={ref}>
+    result: {props.children}
+  </p>
 ))
 Text2.displayName = 'Text2'
