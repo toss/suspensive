d2876b4006c9d15cf26be2985c0ee5e7e646e0c5
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-02-02 02:12:18 +0900

    chore: update

--- websites/visualization/src/app/react/zodSearchParams/page.tsx
+++ websites/visualization/src/app/react/zodSearchParams/page.tsx
@@ -6,6 +6,16 @@ import { useSearchParams } from 'next/navigation'
 import { ZodError, z } from 'zod'
 import { delay } from '~/utils'
 
+const searchParamsSchema = z.object({
+  id: z.coerce
+    .number({
+      invalid_type_error: 'searchParam: id type should be number',
+      required_error: 'searchParam: id is required',
+    })
+    .int('searchParam: id type should be integer')
+    .min(1, 'searchParam: id type should be number bigger than 1'),
+})
+
 export default wrap
   .ErrorBoundary({
     shouldCatch: ZodError,
@@ -28,15 +38,7 @@ export default wrap
   })
   .on(() => {
     const searchParams = useSearchParams()
-    const id = z.coerce
-      .number({
-        invalid_type_error: 'searchParam: id type should be number',
-        required_error: 'searchParam: id is required',
-      })
-      .int('searchParam: id type should be integer')
-      .min(1, 'searchParam: id type should be number bigger than 1')
-      .parse(searchParams.get('id'))
-
+    const { id } = searchParamsSchema.parse(Object.fromEntries(searchParams.entries()))
     const userQuery = useSuspenseQuery({
       queryKey: ['users', id] as const,
       queryFn: ({ queryKey: [, userId] }) => delay(200).then(() => ({ id: userId, name: 'John' })),
