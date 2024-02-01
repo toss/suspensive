304a625a353f2e4fe2ccc9a1384b4b68a129254c
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-02-02 02:37:48 +0900

    fix(react): new internal api `SuspensiveError` (#615)
    fix #598 
    
    new internal api: `SuspensiveError` to assert each hooks position for
    ErrorBoundary, ErrorBoundaryGroup
    SuspensiveError shouldn't be caught by ErrorBoundary. so I updated
    implementation of `<ErrorBoundary/>`
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/suspensive/react/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.

---
+++ .changeset/gorgeous-beans-sniff.md
@@ -0,0 +1,5 @@
+---
+'@suspensive/react': minor
+---
+
+feat(react): add SuspensiveError for useErrorBoundary, useErrorBoundaryFallbackProps, useErrorBoundaryGroup internally
--- packages/react/src/utils/index.ts
+++ packages/react/src/utils/index.ts
@@ -1,4 +1,3 @@
-export { assert } from './assert'
 export { hasResetKeysChanged } from './hasResetKeysChanged'
 export { increase } from './increase'
 export { noop } from './noop'
