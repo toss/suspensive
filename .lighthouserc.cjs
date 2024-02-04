135b704644ce888ac37150aec29bdfcc41d0dba7
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-02-05 02:58:45 +0900

    ci(lighthouse): only for suspensive.org

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -1,8 +1,14 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:4000/', 'http://localhost:4001/', 'http://localhost:4100/'],
-      startServerCommand: 'pnpm start',
+      url: [
+        'http://localhost:4001/',
+        'http://localhost:4001/docs/why/',
+        'https://suspensive.org/',
+        'https://suspensive.org/docs/why/',
+      ],
+      startServerCommand: 'pnpm start --filter=@suspensive/suspensive.org',
+      numberOfRuns: 2,
     },
     upload: {
       target: 'temporary-public-storage',
