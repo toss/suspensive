9171640dd03538ab522ee35a7645aa975b6ac183
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-02-05 04:30:03 +0900

    chore: update

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -1,8 +1,8 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:4001/', 'http://localhost:4001/docs/why/', 'http://localhost:4000/'],
-      startServerCommand: 'pnpm start --filter=@suspensive/suspensive.org --filter=@suspensive/visualization',
+      url: ['http://localhost:4001/', 'http://localhost:4001/docs/why/'],
+      startServerCommand: 'pnpm start --filter=@suspensive/suspensive.org',
       numberOfRuns: 2,
     },
     upload: {
