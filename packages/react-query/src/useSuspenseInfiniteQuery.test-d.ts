53120daa0c9c3f3064d9a1d0dbbe37eae6092697
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-02-14 19:12:13 +0900

    test(react-query): add test cases for hooks with select (#723)
    related https://github.com/suspensive/react/pull/711
    
    # Overview
    
    <!--
        A clear and concise description of what this pr is about.
     -->
    
    @Kanary159357 I made test cases to guarantee that it work
    correctly(useSuspenseQuries + select option)
    Review this Pull Request and approve this please.
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/suspensive/react/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.
    
    ---------
    
    Co-authored-by: John <ddasfasd123@gmail.com>

--- packages/react-query/src/useSuspenseInfiniteQuery.test-d.ts
+++ packages/react-query/src/useSuspenseInfiniteQuery.test-d.ts
@@ -1,51 +1,48 @@
 /* eslint-disable react-hooks/rules-of-hooks */
+import { sleep } from '@suspensive/test-utils'
 import type { InfiniteData } from '@tanstack/react-query'
 import { expectTypeOf } from 'vitest'
 import { useSuspenseInfiniteQuery } from '../dist'
 
 const queryKey = ['key'] as const
-const queryFn = () => 'response' as const
+const queryFn = () => sleep(10).then(() => ({ text: 'response' }) as const)
 const boolean = Math.random() > 0.5
-
-type AwaitedQueryFnReturn = Awaited<ReturnType<typeof queryFn>>
 
 // arg1:queryKey, arg2: queryFn, arg3: options
 expectTypeOf(
   useSuspenseInfiniteQuery(queryKey, queryFn, {
     enabled: true,
   }).data
-).toEqualTypeOf<InfiniteData<AwaitedQueryFnReturn>>()
+).toEqualTypeOf<InfiniteData<Awaited<ReturnType<typeof queryFn>>>>()
 expectTypeOf(
   useSuspenseInfiniteQuery(queryKey, queryFn, {
     enabled: boolean,
   }).data
-).toEqualTypeOf<InfiniteData<AwaitedQueryFnReturn> | undefined>()
+).toEqualTypeOf<InfiniteData<Awaited<ReturnType<typeof queryFn>>> | undefined>()
 expectTypeOf(
   useSuspenseInfiniteQuery(queryKey, queryFn, {
     enabled: false,
   }).data
 ).toEqualTypeOf<undefined>()
-
 // arg1:queryKey, arg2: options
 expectTypeOf(
   useSuspenseInfiniteQuery(queryKey, {
     queryFn,
     enabled: true,
   }).data
-).toEqualTypeOf<InfiniteData<AwaitedQueryFnReturn>>()
+).toEqualTypeOf<InfiniteData<Awaited<ReturnType<typeof queryFn>>>>()
 expectTypeOf(
   useSuspenseInfiniteQuery(queryKey, {
     queryFn,
     enabled: boolean,
   }).data
-).toEqualTypeOf<InfiniteData<AwaitedQueryFnReturn> | undefined>()
+).toEqualTypeOf<InfiniteData<Awaited<ReturnType<typeof queryFn>>> | undefined>()
 expectTypeOf(
   useSuspenseInfiniteQuery(queryKey, {
     queryFn,
     enabled: false,
   }).data
 ).toEqualTypeOf<undefined>()
-
 // arg1: options
 expectTypeOf(
   useSuspenseInfiniteQuery({
@@ -53,14 +50,14 @@ expectTypeOf(
     queryFn,
     enabled: true,
   }).data
-).toEqualTypeOf<InfiniteData<AwaitedQueryFnReturn>>()
+).toEqualTypeOf<InfiniteData<Awaited<ReturnType<typeof queryFn>>>>()
 expectTypeOf(
   useSuspenseInfiniteQuery({
     queryKey,
     queryFn,
     enabled: boolean,
   }).data
-).toEqualTypeOf<InfiniteData<AwaitedQueryFnReturn> | undefined>()
+).toEqualTypeOf<InfiniteData<Awaited<ReturnType<typeof queryFn>>> | undefined>()
 expectTypeOf(
   useSuspenseInfiniteQuery({
     queryKey,
