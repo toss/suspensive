3f2c0d72bf53c00130384480cc1b0ed1addb5eaf
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-02-22 21:54:18 +0900

    feat(*): remove all experimental interface

--- packages/react/src/utility-types/PropsWithDevMode.ts
+++ packages/react/src/utility-types/PropsWithDevMode.ts
@@ -4,8 +4,5 @@ export type PropsWithDevMode<
   TProps extends ComponentProps<ComponentType>,
   TDevModeProps extends Record<string, unknown>,
 > = TProps & {
-  /**
-   * @experimental This is experimental feature.
-   */
   devMode?: TDevModeProps
 }
