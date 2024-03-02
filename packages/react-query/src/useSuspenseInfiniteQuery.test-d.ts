785a51a1b3e3cfaa70169a9a275d172009e6318a
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-03-02 18:03:53 +0900

    feat(react-query): add `<SuspenseQuery/>`, `<SuspenseInfiniteQuery/>` (#775)
    close #771 
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/suspensive/react/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.

--- packages/react-query/src/useSuspenseInfiniteQuery.test-d.ts
+++ packages/react-query/src/useSuspenseInfiniteQuery.test-d.ts
@@ -1,69 +1,60 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import { boolean, queryFn, queryKey } from '@suspensive/test-utils'
+import { queryFn, queryKey } from '@suspensive/test-utils'
 import type { InfiniteData } from '@tanstack/react-query'
 import { describe, expectTypeOf, it } from 'vitest'
-import { useSuspenseInfiniteQuery } from '../dist'
-
-// @ts-expect-error no arg
-useSuspenseInfiniteQuery()
-
-// @ts-expect-error no suspense
-useSuspenseInfiniteQuery({ queryKey, queryFn, suspense: boolean })
-// @ts-expect-error no suspense
-useSuspenseInfiniteQuery(queryKey, { queryFn, suspense: boolean })
-// @ts-expect-error no suspense
-useSuspenseInfiniteQuery(queryKey, queryFn, { suspense: boolean })
-
-// @ts-expect-error no useErrorBoundary
-useSuspenseInfiniteQuery({ queryKey, queryFn, useErrorBoundary: boolean })
-// @ts-expect-error no useErrorBoundary
-useSuspenseInfiniteQuery(queryKey, { queryFn, useErrorBoundary: boolean })
-// @ts-expect-error no useErrorBoundary
-useSuspenseInfiniteQuery(queryKey, queryFn, { useErrorBoundary: boolean })
-
-// @ts-expect-error no enabled
-useSuspenseInfiniteQuery({ queryKey, queryFn, enabled: boolean })
-// @ts-expect-error no enabled
-useSuspenseInfiniteQuery(queryKey, { queryFn, enabled: boolean })
-// @ts-expect-error no enabled
-useSuspenseInfiniteQuery(queryKey, queryFn, { enabled: boolean })
-
-// @ts-expect-error no placeholderData
-useSuspenseInfiniteQuery({ queryKey, queryFn, placeholderData: 'placeholder' })
-// @ts-expect-error no placeholderData
-useSuspenseInfiniteQuery(queryKey, { queryFn, placeholderData: 'placeholder' })
-// @ts-expect-error no placeholderData
-useSuspenseInfiniteQuery(queryKey, queryFn, { placeholderData: 'placeholder' })
-
-// @ts-expect-error no isPlaceholderData
-useSuspenseInfiniteQuery(queryKey, queryFn).isPlaceholderData
-// @ts-expect-error no isPlaceholderData
-useSuspenseInfiniteQuery(queryKey, queryFn, {}).isPlaceholderData
-// @ts-expect-error no isPlaceholderData
-useSuspenseInfiniteQuery(queryKey, { queryFn }).isPlaceholderData
-// @ts-expect-error no isPlaceholderData
-useSuspenseInfiniteQuery({ queryKey, queryFn }).isPlaceholderData
+import { type UseSuspenseInfiniteQueryResult, useSuspenseInfiniteQuery } from '../dist'
 
 describe('useSuspenseInfiniteQuery', () => {
-  it("'s type check", () => {
-    // data is always defined
-    expectTypeOf(useSuspenseInfiniteQuery(queryKey, queryFn).data).toEqualTypeOf<
-      InfiniteData<Awaited<ReturnType<typeof queryFn>>>
-    >()
-    expectTypeOf(useSuspenseInfiniteQuery(queryKey, queryFn, {}).data).toEqualTypeOf<
-      InfiniteData<Awaited<ReturnType<typeof queryFn>>>
-    >()
-    expectTypeOf(useSuspenseInfiniteQuery(queryKey, { queryFn }).data).toEqualTypeOf<
-      InfiniteData<Awaited<ReturnType<typeof queryFn>>>
-    >()
-    expectTypeOf(useSuspenseInfiniteQuery({ queryKey, queryFn }).data).toEqualTypeOf<
-      InfiniteData<Awaited<ReturnType<typeof queryFn>>>
-    >()
+  it('type error', () => {
+    // @ts-expect-error no arg
+    useSuspenseInfiniteQuery()
 
-    // status is always 'success'
-    expectTypeOf(useSuspenseInfiniteQuery(queryKey, queryFn).status).toEqualTypeOf<'success'>()
-    expectTypeOf(useSuspenseInfiniteQuery(queryKey, queryFn, {}).status).toEqualTypeOf<'success'>()
-    expectTypeOf(useSuspenseInfiniteQuery(queryKey, { queryFn }).status).toEqualTypeOf<'success'>()
-    expectTypeOf(useSuspenseInfiniteQuery({ queryKey, queryFn }).status).toEqualTypeOf<'success'>()
+    useSuspenseInfiniteQuery({
+      queryKey,
+      queryFn,
+      // @ts-expect-error no suspense
+      suspense: boolean,
+    })
+    useSuspenseInfiniteQuery({
+      queryKey,
+      queryFn,
+      // @ts-expect-error no useErrorBoundary
+      useErrorBoundary: boolean,
+    })
+    useSuspenseInfiniteQuery({
+      queryKey,
+      queryFn,
+      // @ts-expect-error no enabled
+      enabled: boolean,
+    })
+    useSuspenseInfiniteQuery({
+      queryKey,
+      queryFn,
+      // @ts-expect-error no placeholderData
+      placeholderData: 'placeholder',
+    })
+    useSuspenseInfiniteQuery({
+      queryKey,
+      queryFn,
+      // @ts-expect-error no isPlaceholderData
+    }).isPlaceholderData
+  })
+
+  it('type check', () => {
+    const infiniteQuery = useSuspenseInfiniteQuery({ queryKey, queryFn })
+    expectTypeOf(infiniteQuery).toEqualTypeOf<UseSuspenseInfiniteQueryResult<{ text: string }>>()
+    expectTypeOf(infiniteQuery.data).toEqualTypeOf<InfiniteData<{ text: string }>>()
+    expectTypeOf(infiniteQuery.status).toEqualTypeOf<'success'>()
+
+    const selectedInfiniteQuery = useSuspenseInfiniteQuery({
+      queryKey,
+      queryFn,
+      select: (data) => ({
+        pages: data.pages.map(({ text }) => text),
+        pageParams: data.pageParams,
+      }),
+    })
+    expectTypeOf(selectedInfiniteQuery).toEqualTypeOf<UseSuspenseInfiniteQueryResult<string>>()
+    expectTypeOf(selectedInfiniteQuery.data).toEqualTypeOf<InfiniteData<string>>()
+    expectTypeOf(selectedInfiniteQuery.status).toEqualTypeOf<'success'>()
   })
 })
