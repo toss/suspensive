c9b0d661141cd29455cffdc0e5155a05de9cc657
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-04-21 15:56:23 +0900

    chore(*): add @coauthors/prepare-commit-msg (#857)
    Thanks to @2-NOW
    
    # Overview
    
    <!--
        A clear and concise description of what this pr is about.
     -->
    
    ![Apr-20-2024
    04-14-41](https://github.com/suspensive/react/assets/61593290/7abf0ae9-0f8d-4b69-9a5d-d33b677bb39c)
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/suspensive/react/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.
    
    ---------
    
    Co-authored-by: Lee HyunJae (whale) <71202076+2-NOW@users.noreply.github.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,9 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
+  extends: ['@suspensive/eslint-config-ts'],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: 'tsconfig.json',
+  },
 }
