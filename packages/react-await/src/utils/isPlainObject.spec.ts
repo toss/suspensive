9115c15f60407177bedb7c1a49aadf8b535f61e8
Pengoose <dkedid@naver.com>
2024-05-14 22:22:51 +0900

    test(react, react-await): separate success & failure descriptions (#875)
    # Overview
    
    This pull request aims to improve test clarity by **separating success
    and failure descriptions** into distinct `describe` blocks. I believe
    this change will enhance readability and maintainability by clearly
    distinguishing between different test scenarios.
    
    I would appreciate your feedback on this approach. Thank you! :)
    
    Key changes:
    - Separate `describe` blocks for success and failure cases.
    - Updated test descriptions for clarity.
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/suspensive/react/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.
    
    ---------
    
    Co-authored-by: Jonghyeon Ko <manudeli.ko@gmail.com>

--- packages/react-await/src/utils/isPlainObject.spec.ts
+++ packages/react-await/src/utils/isPlainObject.spec.ts
@@ -1,44 +1,48 @@
 import { isPlainObject } from './isPlainObject'
 
 describe('isPlainObject', () => {
-  it('should return true for a plain object', () => {
-    expect(isPlainObject({})).toBe(true)
+  describe('true cases', () => {
+    it('should return true for a plain object', () => {
+      expect(isPlainObject({})).toBe(true)
+    })
+
+    it('should return true for an object without a constructor', () => {
+      expect(isPlainObject(Object.create(null))).toBe(true)
+    })
   })
 
-  it('should return true for an object without a constructor', () => {
-    expect(isPlainObject(Object.create(null))).toBe(true)
-  })
+  describe('false cases', () => {
+    it('should return false for an array', () => {
+      expect(isPlainObject([])).toBe(false)
+    })
 
-  it('should return false for an array', () => {
-    expect(isPlainObject([])).toBe(false)
-  })
+    it('should return false for a null value', () => {
+      expect(isPlainObject(null)).toBe(false)
+    })
 
-  it('should return false for a null value', () => {
-    expect(isPlainObject(null)).toBe(false)
-  })
+    it('should return false for an undefined value', () => {
+      expect(isPlainObject(undefined)).toBe(false)
+    })
 
-  it('should return false for an undefined value', () => {
-    expect(isPlainObject(undefined)).toBe(false)
-  })
-
-  it('should return false for an object instance without an Object-specific method', () => {
-    class Foo {
-      abc: any
-      constructor() {
-        this.abc = {}
+    it('should return false for an object instance without an Object-specific method', () => {
+      class Foo {
+        abc: any
+        constructor() {
+          this.abc = {}
+        }
       }
-    }
-    expect(isPlainObject(new Foo())).toBe(false)
-  })
+      expect(isPlainObject(new Foo())).toBe(false)
+    })
 
-  it('should return false for an object with a custom prototype', () => {
-    function Graph(this: any) {
-      this.vertices = []
-      this.edges = []
-    }
-    Graph.prototype.addVertex = function (v: any) {
-      this.vertices.push(v)
-    }
-    expect(isPlainObject(Object.create(Graph))).toBe(false)
+    it('should return false for an object with a custom prototype', () => {
+      function Graph(this: any) {
+        this.vertices = []
+        this.edges = []
+      }
+      Graph.prototype.addVertex = function (v: any) {
+        this.vertices.push(v)
+      }
+      expect(isPlainObject(Object.create(Graph))).toBe(false)
+    })
   })
 })
