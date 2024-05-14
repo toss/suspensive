47c6d1fcd0c468cf18255d9b7256ab29b61f0d74
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-05-15 00:32:41 +0900

    chore(*): last versioning before v2  (#876)
    # Overview
    
    <!--
        A clear and concise description of what this pr is about.
     -->
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/suspensive/react/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.
    
    ---------
    
    Co-authored-by: Lee HyunJae (whale) <71202076+2-NOW@users.noreply.github.com>

--- examples/next-streaming-react-query/src/app/layout.tsx
+++ examples/next-streaming-react-query/src/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from 'next'
+import type { ReactNode } from 'react'
 import { Providers } from './providers'
 
 export const metadata: Metadata = {
   title: 'Next HTML Streaming with Suspense',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
--- examples/next-streaming-react-query/src/app/page.tsx
+++ examples/next-streaming-react-query/src/app/page.tsx
@@ -1,25 +1,89 @@
-import { HydrationBoundary, dehydrate } from '@tanstack/react-query'
+'use client'
+
+import { Suspense } from '@suspensive/react'
+import { useQueryClient } from '@tanstack/react-query'
 import Link from 'next/link'
-import { PageClientComponent } from './PageClientComponent'
+import { Text } from '~/app/components/Text'
+import { Text2 } from '~/app/components/Text2'
 import { query } from '~/query'
-import { getQueryClient } from '~/react-query'
+import { SuspenseQuery } from '~/react-query'
 
-export default async function MyPage() {
-  const queryClient = getQueryClient()
-  await Promise.all([
-    queryClient.prefetchQuery(query.text(100)),
-    queryClient.prefetchQuery(query.text(200)),
-    queryClient.prefetchQuery(query.text(300)),
-    queryClient.prefetchQuery(query.text(400)),
-  ])
-
-  const data = queryClient.getQueryData(query.text(100).queryKey)
-  console.log(data)
+export default function Page() {
+  const queryClient = useQueryClient()
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
+    <>
       <Link href="/test">to test page</Link>
-      <PageClientComponent />
-    </HydrationBoundary>
+      <Suspense>
+        <Text ms={100} />
+      </Suspense>
+      <Suspense>
+        <Text ms={200} />
+      </Suspense>
+      <Suspense>
+        <Text ms={300} />
+      </Suspense>
+      <Suspense>
+        <Text ms={400} />
+      </Suspense>
+      <Suspense>
+        <Text ms={500} />
+      </Suspense>
+      <Suspense>
+        <Text ms={600} />
+      </Suspense>
+      <Suspense>
+        <Text ms={700} />
+      </Suspense>
+
+      <button
+        onClick={() => {
+          queryClient.resetQueries()
+        }}
+      >
+        resetQueries all
+      </button>
+
+      <button
+        onClick={() => {
+          queryClient.invalidateQueries(query.text(500))
+        }}
+      >
+        invalidate 500
+      </button>
+
+      <button
+        onClick={() => {
+          queryClient.invalidateQueries(query.text(200))
+        }}
+      >
+        invalidate 200
+      </button>
+
+      <fieldset>
+        <legend>
+          combined <code>Suspense</code>-container
+        </legend>
+        <Suspense>
+          <Text ms={800} />
+          <Text ms={900} />
+          <Text ms={1000} />
+        </Suspense>
+      </fieldset>
+
+      <pre>{`Proposal: <SuspenseQuery /> Component`}</pre>
+      {/* Need to proposal */}
+      <ul>
+        <Suspense>
+          <SuspenseQuery {...query.text(1100)}>{({ data }) => <Text2>{data}</Text2>}</SuspenseQuery>
+        </Suspense>
+        <Suspense>
+          <SuspenseQuery {...query.text(1200)}>{({ data }) => <Text2>{data}</Text2>}</SuspenseQuery>
+        </Suspense>
+        <Suspense>
+          <SuspenseQuery {...query.text(1300)}>{({ data }) => <Text2>{data}</Text2>}</SuspenseQuery>
+        </Suspense>
+      </ul>
+    </>
   )
 }
