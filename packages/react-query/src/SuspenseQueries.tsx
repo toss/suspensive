92f674ad325a4c2f73aa6701c2b6a050b59aad61
Jonghyeon Ko <jonghyeon@toss.im>
2024-06-18 02:06:05 +0900

    feat(react-query): add SuspenseQueries
    Co-authored-by: GwanSik Kim <39869096+gwansikk@users.noreply.github.com>

---
+++ packages/react-query/src/SuspenseQueries.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from 'react'
+import { type SuspenseQueriesOptions, type SuspenseQueriesResults, useSuspenseQueries } from './useSuspenseQueries'
+
+/**
+ * @experimental This is experimental feature.
+ */
+export function SuspenseQueries<T extends any[]>({
+  children,
+  queries,
+}: {
+  queries: readonly [...SuspenseQueriesOptions<T>]
+  children: (queries: SuspenseQueriesResults<T>) => ReactNode
+}) {
+  return <>{children(useSuspenseQueries({ queries }))}</>
+}
