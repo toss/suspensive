65c602aeae386c4cb8eec283e48834e963112a9d
Jonghyeon Ko <jonghyeon@toss.im>
2024-06-18 04:00:36 +0900

    chore(react-query): update SuspenseQueries as public api

---
+++ .changeset/cyan-timers-judge.md
@@ -0,0 +1,5 @@
+---
+'@suspensive/react-query': patch
+---
+
+chore(react-query): SuspenseQueries as public api
--- packages/react-query/src/utils/isVersion.ts
+++
@@ -1,6 +0,0 @@
-export const isVersion = (version: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7) => ({
-  of: (packageVersion: string) => {
-    const [major] = packageVersion.split('.').map(Number)
-    return major === version
-  },
-})
