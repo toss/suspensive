d87a48289244737f368c855d6afa1e85c250d78c
SeongMin Kim <86355699+Collection50@users.noreply.github.com>
2024-06-24 18:26:07 +0900

    fix(react): add `<ClientOnly/>` internally (#964)
    related #528 
    
    # Overview
    
    Implemented the `ClientOnly` component.
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/toss/suspensive/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.
    
    ---------
    
    Co-authored-by: Jonghyeon Ko <jonghyeon@toss.im>

---
+++ packages/react/src/components/ClientOnly.spec.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react'
+import { type Mock } from 'vitest'
+import { useIsClient } from '../hooks/useIsClient'
+import { ClientOnly } from './ClientOnly'
+
+vi.mock('../hooks/useIsClient', () => ({
+  useIsClient: vi.fn(),
+}))
+
+afterEach(() => {
+  vi.clearAllMocks()
+})
+
+describe('<ClientOnly/>', () => {
+  it('renders children when isClient is true', () => {
+    ;(useIsClient as Mock).mockReturnValue(true)
+
+    render(
+      <ClientOnly fallback={<div>Loading...</div>}>
+        <div>Client Content</div>
+      </ClientOnly>
+    )
+
+    expect(screen.getByText('Client Content')).toBeInTheDocument()
+    expect(screen.queryByText('Loading...')).not.toBeInTheDocument()
+  })
+
+  it('renders fallback when isClient is false', () => {
+    ;(useIsClient as Mock).mockReturnValue(false)
+
+    render(
+      <ClientOnly fallback={<div>Loading...</div>}>
+        <div>Client Content</div>
+      </ClientOnly>
+    )
+
+    expect(screen.getByText('Loading...')).toBeInTheDocument()
+    expect(screen.queryByText('Client Content')).not.toBeInTheDocument()
+  })
+})
