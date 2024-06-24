b6be3c87860f89b2814845a9e08486c2068ad614
jungwoo3490 <jungwoo3490@naver.com>
2024-06-25 05:59:18 +0900

    chore(react-query): update package.json & tsup.config.js

--- packages/react-query/tsup.config.ts
+++ packages/react-query/tsup.config.ts
@@ -5,7 +5,7 @@ export default defineConfig([
   options,
   {
     target: ['chrome51', 'firefox53', 'edge18', 'safari11', 'ios11', 'opera38', 'es6', 'node14'],
-    entry: ['src/scripts/**/*.{ts,tsx}', '!**/*.{spec,test,test-d,bench}.*'],
+    entry: ['src/scripts/*.{ts,tsx}', 'src/scripts/**/*.{ts,tsx}', '!**/*.{spec,test,test-d,bench}.*'],
     format: ['cjs'],
     sourcemap: true,
     dts: true,
