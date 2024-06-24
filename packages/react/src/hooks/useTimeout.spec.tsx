2da0d2c16cca408be35892b2f4a9db884615b01f
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-06-24 18:37:16 +0900

    fix(react): improved useTimeout and added usePreservedCallback (#966)
    # Overview
    
    @manudeli Hello! 👋
    
    Currently, `useTimeout` hook is written to call the changed function
    when the callback function changes.
    However, in practice, we're not actually calling the changed function,
    even if the function changes in the middle.
    
    This is because the only value we currently depend on for `useEffect` is
    `ms`.
    Adding `ref` to the dependency array doesn't solve this problem because
    it's not tracked.
    
    These problems can be solved by using hooks like `usePreservedCallback`
    in `toss/slash`, which can preserve the reference of the callback
    function, while at the same time ensuring the function is up-to-date.
    
    -
    [usePreservedCallback](https://github.com/toss/slash/blob/main/packages/react/react/src/hooks/usePreservedCallback.ts)
    
    Since the `ref` keeps updating during its lifecycle, when calling the
    function returned by `useCallback`, the latest function will be called
    by the `closure`.
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/toss/suspensive/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.
    
    ---------
    
    Co-authored-by: Jonghyeon Ko <jonghyeon@toss.im>

--- packages/react/src/hooks/useTimeout.spec.tsx
+++ packages/react/src/hooks/useTimeout.spec.tsx
@@ -16,6 +16,25 @@ describe('useTimeout', () => {
     expect(spy).toHaveBeenCalledTimes(1)
   })
 
+  it('should call the modified function when the given function is changed', async () => {
+    const timeoutMockFn1 = vi.fn()
+    const timeoutMockFn2 = vi.fn()
+
+    const { rerender } = renderHook(({ fn }) => useTimeout(fn, ms('0.1s')), {
+      initialProps: { fn: timeoutMockFn1 },
+    })
+
+    rerender({ fn: timeoutMockFn2 })
+
+    expect(timeoutMockFn1).toHaveBeenCalledTimes(0)
+    expect(timeoutMockFn2).toHaveBeenCalledTimes(0)
+
+    await sleep(ms('0.1s'))
+
+    expect(timeoutMockFn1).toHaveBeenCalledTimes(0)
+    expect(timeoutMockFn2).toHaveBeenCalledTimes(1)
+  })
+
   it('should not re-call callback received as argument even if component using this hook is rerendered', async () => {
     const TestComponent = () => {
       const [number, setNumber] = useState(0)
