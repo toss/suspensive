4352b03f6d3e7a9f51c12ca62fde1e3ded604804
Collection50 <fkaus65@naver.com>
2024-06-24 17:38:41 +0900

    fix: add ClientOnly interface

--- packages/react/src/components/ClientOnly.tsx
+++ packages/react/src/components/ClientOnly.tsx
@@ -1,7 +1,12 @@
-import { type SuspenseProps } from 'react'
+import type { ReactNode } from 'react'
 import { useIsClient } from './useIsClient'
 
-export const ClientOnly = ({ children, fallback }: SuspenseProps) => {
+interface ClientOnlyProp {
+  children: ReactNode
+  fallback?: ReactNode
+}
+
+export const ClientOnly = ({ children, fallback }: ClientOnlyProp) => {
   const isClient = useIsClient()
 
   return isClient ? <>{children}</> : <>{fallback}</>
