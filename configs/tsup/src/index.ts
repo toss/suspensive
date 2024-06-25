0a68b7c0bd8e10563df786edbb18e88e06f46375
Jonghyeon Ko <jonghyeon@toss.im>
2024-06-25 11:39:29 +0900

    fix(react-query): update setup

--- configs/tsup/src/index.ts
+++ configs/tsup/src/index.ts
@@ -5,6 +5,7 @@ export const options: Options = {
   format: ['cjs', 'esm'],
   target: ['chrome51', 'firefox53', 'edge18', 'safari11', 'ios11', 'opera38', 'es6', 'node14'],
   entry: ['src/*.{ts,tsx}', '!**/*.{spec,test,test-d,bench}.*'],
+  outDir: 'dist',
   sourcemap: true,
   dts: true,
 }
--- packages/react-query/tsup.config.ts
+++ packages/react-query/tsup.config.ts
@@ -6,6 +6,7 @@ export default defineConfig([
   {
     target: ['chrome51', 'firefox53', 'edge18', 'safari11', 'ios11', 'opera38', 'es6', 'node14'],
     entry: ['src/scripts/*.{ts,tsx}', 'src/scripts/**/*.{ts,tsx}', '!**/*.{spec,test,test-d,bench}.*'],
+    outDir: 'dist/scripts',
     format: ['cjs'],
     sourcemap: true,
     dts: true,
