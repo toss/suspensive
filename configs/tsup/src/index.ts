63c49cc021539c04142250bb53962a511df1a05e
Jonghyeon Ko <jonghyeon@toss.im>
2024-06-25 13:57:40 +0900

    fix(react-query): remove unnecessary file

--- configs/tsup/src/index.ts
+++ configs/tsup/src/index.ts
@@ -1,6 +1,7 @@
 import type { Options } from 'tsup'
 
 export const options: Options = {
+  clean: true,
   banner: { js: '"use client"' },
   format: ['cjs', 'esm'],
   target: ['chrome51', 'firefox53', 'edge18', 'safari11', 'ios11', 'opera38', 'es6', 'node14'],
--- packages/react-query/tsup.config.ts
+++ packages/react-query/tsup.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig([
   options,
   {
+    clean: true,
     target: ['chrome51', 'firefox53', 'edge18', 'safari11', 'ios11', 'opera38', 'es6', 'node14'],
     entry: ['src/scripts/*.{ts,tsx}', 'src/scripts/**/*.{ts,tsx}', '!**/*.{spec,test,test-d,bench}.*'],
     outDir: 'dist/scripts',
