339892059daa74a0f76914dc506db4602c3e4614
GwanSik Kim <Seorit98@gmail.com>
2024-06-26 18:52:37 +0900

    refactor(react-query): update entry point in scriptOptions
    Co-authored-by: Jonghyeon Ko <jonghyeon@toss.im>

--- configs/tsup/src/index.ts
+++ configs/tsup/src/index.ts
@@ -15,7 +15,7 @@ export const scriptOptions: Options = {
   clean: true,
   format: ['cjs'],
   target: ['chrome51', 'firefox53', 'edge18', 'safari11', 'ios11', 'opera38', 'es6', 'node14'],
-  entry: ['src/scripts/**/*.{ts,tsx}', '!**/*.{spec,test,test-d,bench}.*'],
+  entry: ['src/scripts/*.{ts,tsx}', '!**/*.{spec,test,test-d,bench}.*'],
   outDir: 'dist/scripts',
   sourcemap: true,
 }
