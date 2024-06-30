6b073b4a9c51eb5ac65e1e24c45e36fb477e5d3b
gwansikk <Seorit98@gmail.com>
2024-06-30 23:18:26 +0900

    refactor(react-query): update CLI tests to use execFileSync

--- packages/react-query/src/scripts/cli.test.ts
+++ packages/react-query/src/scripts/cli.test.ts
@@ -1,33 +1,32 @@
-import { execSync } from 'child_process'
+import { execFileSync } from 'child_process'
 import path from 'path'
 import packageJson from '../../package.json'
 
 const cliPath = path.resolve(__dirname, '../../dist/scripts/cli.cjs')
-const command = (command: string) => `node ${cliPath} ${command}`
 
 describe('cli', () => {
   it('should display the correct version when using the -v flag', () => {
-    const result = execSync(command('-v')).toString()
+    const result = execFileSync('node', [cliPath, '-v']).toString()
     expect(result.trim()).toBe(packageJson.version)
   })
 
   it('should display the help message when using the -h flag', () => {
-    const result = execSync(command('-h')).toString()
+    const result = execFileSync('node', [cliPath, '-h']).toString()
     expect(result).toContain('Usage: @suspensive/react-query [options] [command]')
   })
 
   it('should display the status of the packages', () => {
-    const result = execSync(command('status')).toString()
+    const result = execFileSync('node', [cliPath, 'status']).toString()
     expect(result).toContain(`[@suspensive/react-query] v2.3.0 (4)\n[@tanstack/react-query] v4.36.1`)
   })
 
   it('should switch to the specified version when using the switch command', () => {
-    const result = execSync(command('switch 5')).toString()
+    const result = execFileSync('node', [cliPath, 'switch', '5']).toString()
     expect(result).toContain('[@suspensive/react-query] switched to version v5')
   })
 
   it('should fix the version to the compatible version when using the fix command', () => {
-    const result = execSync(command('fix')).toString()
+    const result = execFileSync('node', [cliPath, 'fix']).toString()
     expect(result).toContain(
       '[@suspensive/react-query] Switching to the compatible version...\n[@suspensive/react-query] switched to version v4'
     )
