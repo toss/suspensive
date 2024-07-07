360308c3c26006a1013f02d49e4a953a10661681
gwansikk <Seorit98@gmail.com>
2024-07-07 21:10:52 +0900

    feat(react-query): add infiniteQueryOptions
    Co-authored-by: Juhyeok Kang <23312485+kangju2000@users.noreply.github.com>

---
+++ packages/react-query-4/src/infiniteQueryOptions.ts
@@ -0,0 +1,74 @@
+import type { InfiniteData, QueryKey, UseInfiniteQueryOptions } from '@tanstack/react-query'
+import type { OmitKeyof, RequiredKeyof } from './utility-types'
+
+export type SelectedInfiniteOptions<
+  TQueryFnData,
+  TError = unknown,
+  TData = InfiniteData<TQueryFnData>,
+  TQueryKey extends QueryKey = QueryKey,
+> = RequiredKeyof<
+  OmitKeyof<
+    UseInfiniteQueryOptions<TQueryFnData, TError, TData, TQueryFnData, TQueryKey>,
+    | 'queryKeyHashFn'
+    | '_defaulted'
+    | 'behavior'
+    | 'structuralSharing'
+    | 'isDataEqual'
+    | 'onSuccess'
+    | 'onError'
+    | 'onSettled'
+    | 'enabled'
+    | 'refetchInterval'
+    | 'initialData'
+  >,
+  'queryKey' | 'queryFn'
+> & {
+  select: (data: InfiniteData<TQueryFnData>) => InfiniteData<TData>
+}
+
+export type UnSelectedInfiniteOptions<
+  TQueryFnData,
+  TError = unknown,
+  TData = InfiniteData<TQueryFnData>,
+  TQueryKey extends QueryKey = QueryKey,
+> = RequiredKeyof<
+  OmitKeyof<
+    UseInfiniteQueryOptions<TQueryFnData, TError, TData, TQueryFnData, TQueryKey>,
+    | 'queryKeyHashFn'
+    | '_defaulted'
+    | 'behavior'
+    | 'structuralSharing'
+    | 'isDataEqual'
+    | 'onSuccess'
+    | 'onError'
+    | 'onSettled'
+    | 'enabled'
+    | 'refetchInterval'
+    | 'initialData'
+  >,
+  'queryKey' | 'queryFn'
+> & {
+  select?: undefined
+}
+
+export function infiniteQueryOptions<
+  TQueryFnData,
+  TError = unknown,
+  TData = InfiniteData<TQueryFnData>,
+  TQueryKey extends QueryKey = QueryKey,
+>(
+  options: SelectedInfiniteOptions<TQueryFnData, TError, TData, TQueryKey>
+): SelectedInfiniteOptions<TQueryFnData, TError, TData, TQueryKey>
+
+export function infiniteQueryOptions<
+  TQueryFnData,
+  TError = unknown,
+  TData = InfiniteData<TQueryFnData>,
+  TQueryKey extends QueryKey = QueryKey,
+>(
+  options: UnSelectedInfiniteOptions<TQueryFnData, TError, TData, TQueryKey>
+): UnSelectedInfiniteOptions<TQueryFnData, TError, TData, TQueryKey>
+
+export function infiniteQueryOptions(options: unknown) {
+  return options
+}
