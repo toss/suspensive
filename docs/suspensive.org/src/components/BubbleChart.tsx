9ca61f8af33ac48ef4eb1fd39f60efa360216053
Jonghyeon Ko <jonghyeon@toss.im>
2024-07-09 03:53:23 +0900

    fix(suspensive.org): add fallback for response with 202 status code (#1056)
    close #1051 
    
    # Overview
    
    <!--
        A clear and concise description of what this pr is about.
     -->
    
    @saul-atomrigs I added fallback original image for GitHub api response
    with 202, 403 status code
    And I use @suspensive/react, @suspensive/react-query to refactor your
    code.
    
    ![Jul-09-2024
    03-46-29](https://github.com/toss/suspensive/assets/61593290/d533d866-3177-440d-9d63-30a95e6f51e0)
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/toss/suspensive/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.
    
    ---------
    
    Co-authored-by: Sol Lee <82362278+saul-atomrigs@users.noreply.github.com>

--- docs/suspensive.org/src/components/BubbleChart.tsx
+++ docs/suspensive.org/src/components/BubbleChart.tsx
@@ -1,6 +1,115 @@
+import { Suspense } from '@suspensive/react'
+import { QueryErrorBoundary, SuspenseQuery, queryOptions } from '@suspensive/react-query-4'
 import * as d3 from 'd3'
+import Link from 'next/link'
 import React, { useEffect, useRef } from 'react'
-import useFetchContributors from '../lib/hooks/useFetchContributors'
+
+const contributorsQueryOptions = () =>
+  queryOptions({
+    queryKey: ['contributors'],
+    queryFn: () =>
+      fetch('https://api.github.com/repos/toss/suspensive/stats/contributors').then(async (res) => {
+        if (res.ok) {
+          const data = await res.json()
+          return data as
+            | {
+                author: {
+                  login: string
+                  id: number
+                  node_id: string
+                  avatar_url: string
+                  gravatar_id: string
+                  url: string
+                  html_url: string
+                  followers_url: string
+                  following_url: string
+                  gists_url: string
+                  starred_url: string
+                  subscriptions_url: string
+                  organizations_url: string
+                  repos_url: string
+                  events_url: string
+                  received_events_url: string
+                  type: string
+                  site_admin: boolean
+                }
+                total: number
+                weeks: {
+                  w: number // Week timestamp in epoch seconds
+                  a: number // Number of additions
+                  d: number // Number of deletions
+                  c: number // Number of commits
+                }[]
+              }[]
+            | undefined
+        }
+        throw new Error('Failed to fetch contributors')
+      }),
+  })
+
+export const BubbleChart = () => (
+  <QueryErrorBoundary
+    fallback={
+      <Link href="https://github.com/toss/suspensive/graphs/contributors">
+        <br />
+        <img
+          src="https://contrib.rocks/image?repo=toss/suspensive"
+          alt="contributors"
+          data-canonical-src="https://contrib.rocks/image?repo=toss/suspensive"
+          style={{ maxWidth: '100%' }}
+        />
+      </Link>
+    }
+  >
+    <Suspense clientOnly fallback={<></>}>
+      <SuspenseQuery {...contributorsQueryOptions()}>
+        {({ data }) => {
+          const chartData = data
+            ?.filter(
+              ({ author }) => !['github-actions[bot]', 'dependabot[bot]', 'renovate[bot]'].includes(author.login)
+            )
+            .map(({ author, total }) => ({
+              name: author.login,
+              value: total,
+              avatar: author.avatar_url,
+              htmlUrl: author.html_url,
+            }))
+
+          if (chartData) {
+            return (
+              <>
+                <div className="flex w-[100%] items-center justify-center overflow-visible sm:hidden md:hidden lg:hidden">
+                  <BubbleChartSize chartData={chartData} height={400} width={400} padding={2} />
+                </div>
+                <div className="hidden w-[100%] items-center justify-center overflow-visible sm:flex md:hidden lg:hidden">
+                  <BubbleChartSize chartData={chartData} height={630} width={630} padding={6} />
+                </div>
+                <div className="hidden w-[100%] items-center justify-center overflow-visible sm:hidden md:flex lg:hidden">
+                  <BubbleChartSize chartData={chartData} height={560} width={560} padding={4} />
+                </div>
+                <div className="hidden w-[100%] items-center justify-center overflow-visible sm:hidden md:hidden lg:flex">
+                  <BubbleChartSize chartData={chartData} height={760} width={760} padding={8} />
+                </div>
+              </>
+            )
+          }
+
+          return (
+            <Link href="https://github.com/toss/suspensive/graphs/contributors">
+              <br />
+              <img
+                src="https://contrib.rocks/image?repo=toss/suspensive"
+                alt="contributors"
+                data-canonical-src="https://contrib.rocks/image?repo=toss/suspensive"
+                style={{ maxWidth: '100%' }}
+              />
+            </Link>
+          )
+        }}
+      </SuspenseQuery>
+    </Suspense>
+  </QueryErrorBoundary>
+)
 
 type Node = {
   name: string
@@ -9,39 +118,6 @@ type Node = {
   htmlUrl: string
   children?: Node[]
 }
-
-export const BubbleChart = () => {
-  const result = useFetchContributors()
-
-  if (!result.isSuccess) {
-    return null
-  }
-
-  const chartData = result.data.map((contributor) => ({
-    name: contributor.author.login,
-    value: contributor.total,
-    avatar: contributor.author.avatar_url,
-    htmlUrl: contributor.author.html_url,
-  }))
-
-  return (
-    <>
-      <div className="flex w-[100%] items-center justify-center overflow-visible sm:hidden md:hidden lg:hidden">
-        <BubbleChartSize chartData={chartData} height={400} width={400} padding={2} />
-      </div>
-      <div className="hidden w-[100%] items-center justify-center overflow-visible sm:flex md:hidden lg:hidden">
-        <BubbleChartSize chartData={chartData} height={630} width={630} padding={6} />
-      </div>
-      <div className="hidden w-[100%] items-center justify-center overflow-visible sm:hidden md:flex lg:hidden">
-        <BubbleChartSize chartData={chartData} height={560} width={560} padding={4} />
-      </div>
-      <div className="hidden w-[100%] items-center justify-center overflow-visible sm:hidden md:hidden lg:flex">
-        <BubbleChartSize chartData={chartData} height={760} width={760} padding={8} />
-      </div>
-    </>
-  )
-}
-
 const BubbleChartSize = (props: { chartData: Array<Node>; width: number; height: number; padding: number }) => {
   const svgRef = useRef<SVGSVGElement | null>(null)
 
@@ -92,7 +168,7 @@ const BubbleChartSize = (props: { chartData: Array<Node>; width: number; height:
       .attr('height', (d) => d.r * 2)
       .attr('href', (d: d3.HierarchyCircularNode<Node>) => d.data.avatar)
       .attr('clip-path', (d) => `circle(${d.r * 1}px at ${d.r * 1}px ${d.r * 1}px)`)
-  }, [props.chartData, props.height, props.width])
+  }, [props.chartData, props.height, props.width, props.padding])
 
   return <svg ref={svgRef} />
 }
