a0f36d8f36b8a92e3df88aba677f3294d741d492
김석진 <sj01048260279@gmail.com>
2024-07-12 12:17:25 +0900

    chore(promise): fix key -> promiseKey

--- packages/promise/src/PromiseCache.ts
+++ packages/promise/src/PromiseCache.ts
@@ -18,31 +18,31 @@ export class PromiseCache {
   private cache = new Map<ReturnType<typeof hashKey>, PromiseCacheState>()
   private syncsMap = new Map<ReturnType<typeof hashKey>, Sync[]>()
 
-  public reset = (key?: Key) => {
-    if (key === undefined || key.length === 0) {
+  public reset = (promiseKey?: Key) => {
+    if (promiseKey === undefined || promiseKey.length === 0) {
       this.cache.clear()
       this.syncSubscribers()
       return
     }
 
-    const hashedKey = hashKey(key)
+    const hashedKey = hashKey(promiseKey)
 
     if (this.cache.has(hashedKey)) {
       // TODO: reset with key index hierarchy
       this.cache.delete(hashedKey)
     }
-    this.syncSubscribers(key)
+    this.syncSubscribers(promiseKey)
   }
 
-  public clearError = (key?: Key) => {
-    if (key === undefined || key.length === 0) {
+  public clearError = (promiseKey?: Key) => {
+    if (promiseKey === undefined || promiseKey.length === 0) {
       this.cache.forEach((value, key, map) => {
         map.set(key, { ...value, promise: undefined, error: undefined })
       })
       return
     }
 
-    const hashedKey = hashKey(key)
+    const hashedKey = hashKey(promiseKey)
     const promiseCacheState = this.cache.get(hashedKey)
     if (promiseCacheState) {
       // TODO: clearError with key index hierarchy
@@ -50,13 +50,13 @@ export class PromiseCache {
     }
   }
 
-  public invalidate(key: Key) {
-    const hashedKey = hashKey(key)
+  public invalidate(promiseKey: Key) {
+    const hashedKey = hashKey(promiseKey)
 
     if (this.cache.has(hashedKey)) {
       this.cache.delete(hashedKey)
     }
-    this.syncSubscriber(key)
+    this.syncSubscriber(promiseKey)
   }
 
   public suspend = <TData, TKey extends Key = Key>({
@@ -97,22 +97,22 @@ export class PromiseCache {
     throw newPromiseCache.promise
   }
 
-  public getData = (key: Key) => this.cache.get(hashKey(key))?.data
-  public getError = (key: Key) => this.cache.get(hashKey(key))?.error
+  public getData = (promiseKey: Key) => this.cache.get(hashKey(promiseKey))?.data
+  public getError = (promiseKey: Key) => this.cache.get(hashKey(promiseKey))?.error
 
-  public subscribe(key: Key, syncSubscriber: Sync) {
-    const hashedKey = hashKey(key)
+  public subscribe(promiseKey: Key, syncSubscriber: Sync) {
+    const hashedKey = hashKey(promiseKey)
     const syncs = this.syncsMap.get(hashedKey)
     this.syncsMap.set(hashedKey, [...(syncs ?? []), syncSubscriber])
 
     const subscribed = {
-      unsubscribe: () => this.unsubscribe(key, syncSubscriber),
+      unsubscribe: () => this.unsubscribe(promiseKey, syncSubscriber),
     }
     return subscribed
   }
 
-  public unsubscribe(key: Key, syncSubscriber: Sync) {
-    const hashedKey = hashKey(key)
+  public unsubscribe(promiseKey: Key, syncSubscriber: Sync) {
+    const hashedKey = hashKey(promiseKey)
     const syncs = this.syncsMap.get(hashedKey)
 
     if (syncs) {
@@ -123,16 +123,16 @@ export class PromiseCache {
     }
   }
 
-  private syncSubscribers = (key?: Key) => {
-    const hashedKey = key ? hashKey(key) : undefined
+  private syncSubscribers = (promiseKey?: Key) => {
+    const hashedKey = promiseKey ? hashKey(promiseKey) : undefined
 
     return hashedKey
       ? this.syncsMap.get(hashedKey)?.forEach((sync) => sync())
       : this.syncsMap.forEach((syncs) => syncs.forEach((sync) => sync()))
   }
 
-  private syncSubscriber = (key: Key) => {
-    const hashedKey = hashKey(key)
+  private syncSubscriber = (promiseKey: Key) => {
+    const hashedKey = hashKey(promiseKey)
 
     if (hashedKey) {
       this.syncsMap.get(hashedKey)?.forEach((sync) => sync())
