d7785fa354332c982a67e24426366e1884c43deb
gwansikk <Seorit98@gmail.com>
2024-07-19 19:39:47 +0900

    refactor(react-query): update type in loadModule

--- packages/react-query/src/scripts/utils/loadModule.ts
+++ packages/react-query/src/scripts/utils/loadModule.ts
@@ -1,7 +1,7 @@
 export function loadModule<T>(name: string): T | undefined {
   try {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return require(name)
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    return require(name) as T
   } catch (e) {
     return undefined
   }
