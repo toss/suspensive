87c69c2663f1dee7e40c96c138149cc01f16d94f
gwansikk <Seorit98@gmail.com>
2024-07-19 19:56:55 +0900

    test(react-query): remove side effect test in package spec

--- packages/react-query/src/scripts/utils/package.spec.ts
+++ packages/react-query/src/scripts/utils/package.spec.ts
@@ -1,7 +1,6 @@
 import tanStackReactQueryPackageJson from '@tanstack/react-query/package.json'
 import packageJson from '../../../package.json'
 import {
-  getIndexFileContent,
   getPackageJson,
   getTanStackReactQueryPackageJson,
   getTargetSuspensiveReactQueryAPIs,
@@ -25,17 +24,6 @@ describe('package', () => {
     expect(result.name).toBe(tanStackReactQueryPackageJson.name)
     expect(result.description).toBe(tanStackReactQueryPackageJson.description)
     expect(result.version).toBe(tanStackReactQueryPackageJson.version)
-  })
-
-  it('should get the content of the index file', () => {
-    const indexFileContent = getIndexFileContent()
-
-    expect(indexFileContent).toBeDefined()
-    expect(indexFileContent).toContain(`"use client"
-
-// src/v4.ts
-export * from "@suspensive/react-query-4";
-//# sourceMappingURL=v4.js.map`)
   })
 
   it('should get the target @suspensive/react-query version from the index file content', () => {
