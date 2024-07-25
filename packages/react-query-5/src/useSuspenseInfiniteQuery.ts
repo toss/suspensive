02ca471bfc1a4b141062679bf14fe2018bee4b5d
gwansikk <Seorit98@gmail.com>
2024-07-26 00:12:23 +0900

    feat(react-query): add re-export tanstack query functions

--- packages/react-query-5/src/useSuspenseInfiniteQuery.ts
+++ packages/react-query-5/src/useSuspenseInfiniteQuery.ts
@@ -1,4 +1,30 @@
-import { useSuspenseInfiniteQuery as original_useSuspenseInfiniteQuery } from '@tanstack/react-query'
+import {
+  type UseSuspenseInfiniteQueryOptions as original_UseSuspenseInfiniteQueryOptions,
+  type UseSuspenseInfiniteQueryResult as original_UseSuspenseInfiniteQueryResult,
+  useSuspenseInfiniteQuery as original_useSuspenseInfiniteQuery,
+} from '@tanstack/react-query'
+
+/**
+ * This feature is officially supported in \@tanstack/react-query@5, You can proceed with the migration.
+ * @deprecated Use `UseSuspenseInfiniteQueryOptions` from \@tanstack/react-query@5
+ * @example
+ * ```diff
+ * - import type { UseSuspenseInfiniteQueryOptions } from '@suspensive/react-query'
+ * + import type { UseSuspenseInfiniteQueryOptions } from '@tanstack/react-query'
+ * ```
+ */
+export type UseSuspenseInfiniteQueryOptions = original_UseSuspenseInfiniteQueryOptions
+
+/**
+ * This feature is officially supported in \@tanstack/react-query@5, You can proceed with the migration.
+ * @deprecated Use `UseSuspenseInfiniteQueryResult` from \@tanstack/react-query@5
+ * @example
+ * ```diff
+ * - import type { UseSuspenseInfiniteQueryResult } from '@suspensive/react-query'
+ * + import type { UseSuspenseInfiniteQueryResult } from '@tanstack/react-query'
+ * ```
+ */
+export type UseSuspenseInfiniteQueryResult = original_UseSuspenseInfiniteQueryResult
 
 /**
  * This feature is officially supported in \@tanstack/react-query@5, You can proceed with the migration.
