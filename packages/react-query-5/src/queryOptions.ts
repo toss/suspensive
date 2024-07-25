c1f873f9e62c6fb2f410a24c82d0427669016f2a
GwanSik Kim <Seorit98@gmail.com>
2024-07-25 23:00:12 +0900

    Update packages/react-query-5/src/queryOptions.ts
    Co-authored-by: Jonghyeon Ko <jonghyeon@toss.im>

--- packages/react-query-5/src/queryOptions.ts
+++ packages/react-query-5/src/queryOptions.ts
@@ -1,45 +1,13 @@
-import type {
-  DataTag,
-  DefaultError,
-  DefinedInitialDataOptions,
-  QueryKey,
-  UndefinedInitialDataOptions,
-} from '@tanstack/react-query'
+import { queryOptions as original_queryOptions } from '@tanstack/react-query'
 
 /**
  * This feature is officially supported in \@tanstack/react-query@5, You can proceed with the migration.
  * @deprecated Use `queryOptions` from \@tanstack/react-query@5
+ * @example
+ * ```diff
+ * - import { queryOptions } from '@suspensive/react-query'
+ * + import { queryOptions } from '@tanstack/react-query'
+ * ```
  */
-export function queryOptions<
-  TQueryFnData = unknown,
-  TError = DefaultError,
-  TData = TQueryFnData,
-  TQueryKey extends QueryKey = QueryKey,
->(
-  options: DefinedInitialDataOptions<TQueryFnData, TError, TData, TQueryKey>
-): DefinedInitialDataOptions<TQueryFnData, TError, TData, TQueryKey> & {
-  queryKey: DataTag<TQueryKey, TQueryFnData>
-}
+export const queryOptions = original_queryOptions
 
-/**
- * This feature is officially supported in \@tanstack/react-query@5, You can proceed with the migration.
- * @deprecated Use `queryOptions` from \@tanstack/react-query@5
- */
-export function queryOptions<
-  TQueryFnData = unknown,
-  TError = DefaultError,
-  TData = TQueryFnData,
-  TQueryKey extends QueryKey = QueryKey,
->(
-  options: UndefinedInitialDataOptions<TQueryFnData, TError, TData, TQueryKey>
-): UndefinedInitialDataOptions<TQueryFnData, TError, TData, TQueryKey> & {
-  queryKey: DataTag<TQueryKey, TQueryFnData>
-}
-
-/**
- * This feature is officially supported in \@tanstack/react-query@5, You can proceed with the migration.
- * @deprecated Use `queryOptions` from \@tanstack/react-query@5
- */
-export function queryOptions(options: unknown) {
-  return options
-}
