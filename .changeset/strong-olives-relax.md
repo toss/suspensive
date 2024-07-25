314410edadee92c10727fb825f2f2666a2bcd6ea
GwanSik Kim <Seorit98@gmail.com>
2024-07-26 00:36:14 +0900

    feat(react-query): add re-export tanstack query functions (#1138)
    # Overview
    
    - https://github.com/toss/suspensive/issues/1096
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/toss/suspensive/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.
    
    ---------
    
    Co-authored-by: Jonghyeon Ko <jonghyeon@toss.im>

---
+++ .changeset/strong-olives-relax.md
@@ -0,0 +1,5 @@
+---
+"@suspensive/react-query-5": minor
+---
+
+feat(react-query): re-export tanstack query functions
--- packages/react-query-5/src/index.ts
+++ packages/react-query-5/src/index.ts
@@ -1,3 +1,12 @@
+export { queryOptions } from './queryOptions'
+export { infiniteQueryOptions } from './infiniteQueryOptions'
+export { useSuspenseQuery } from './useSuspenseQuery'
+export type { UseSuspenseQueryOptions, UseSuspenseQueryResult } from './useSuspenseQuery'
+export { useSuspenseQueries } from './useSuspenseQueries'
+export type { SuspenseQueriesOptions, SuspenseQueriesResults } from './useSuspenseQueries'
+export { useSuspenseInfiniteQuery } from './useSuspenseInfiniteQuery'
+export type { UseSuspenseInfiniteQueryOptions, UseSuspenseInfiniteQueryResult } from './useSuspenseInfiniteQuery'
+
 export { SuspenseQuery } from './SuspenseQuery'
 export { SuspenseQueries } from './SuspenseQueries'
 export { SuspenseInfiniteQuery } from './SuspenseInfiniteQuery'
