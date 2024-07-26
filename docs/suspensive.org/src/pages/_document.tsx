42ab7ba0599579ac146641103a9fdb72abffd20c
kang <kangju2000@naver.com>
2024-07-26 16:52:56 +0900

    chore: add _document.tsx to include Sandpack CSS

---
+++ docs/suspensive.org/src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { getSandpackCssText } from '@codesandbox/sandpack-react'
+import { Head, Html, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <Head>
+        <style dangerouslySetInnerHTML={{ __html: getSandpackCssText() }} id="sandpack" key="sandpack-css" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
