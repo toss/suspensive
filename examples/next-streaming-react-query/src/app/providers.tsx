48cc37fffac18e5f9f16ae5a0ab6bd19c05cc352
Jonghyeon Ko <jonghyeon@toss.im>
2024-07-29 01:31:00 +0900

    docs: update

--- examples/next-streaming-react-query/src/app/providers.tsx
+++ examples/next-streaming-react-query/src/app/providers.tsx
@@ -1,37 +1,21 @@
 'use client'
 
-import { Suspensive, SuspensiveProvider } from '@suspensive/react'
+import { DefaultProps, DefaultPropsProvider } from '@suspensive/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental'
 import { type ReactNode, useState } from 'react'
 
 export function Providers(props: { children: ReactNode }) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 5 * 1000,
-          },
-        },
-      })
-  )
-  const [suspensive] = useState(
-    () =>
-      new Suspensive({
-        defaultProps: {
-          suspense: { fallback: <div>loading...</div> },
-        },
-      })
-  )
+  const queryClient = useState(() => new QueryClient({ defaultOptions: { queries: { staleTime: 5 * 1000 } } }))[0]
+  const defaultProps = useState(() => new DefaultProps({ suspense: { fallback: <div>loading...</div> } }))[0]
 
   return (
-    <SuspensiveProvider value={suspensive}>
+    <DefaultPropsProvider defaultProps={defaultProps}>
       <QueryClientProvider client={queryClient}>
         <ReactQueryStreamedHydration>{props.children}</ReactQueryStreamedHydration>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
-    </SuspensiveProvider>
+    </DefaultPropsProvider>
   )
 }
--- websites/visualization/src/app/providers.tsx
+++ websites/visualization/src/app/providers.tsx
@@ -1,47 +1,25 @@
 'use client'
 
 import { CacheStore, CacheStoreProvider } from '@suspensive/cache'
-import { DevMode, Suspensive, SuspensiveProvider } from '@suspensive/react'
+import { DefaultProps, DefaultPropsProvider } from '@suspensive/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { type PropsWithChildren, useState } from 'react'
 import { Spinner } from '~/components/uis'
 
 export const Providers = ({ children }: PropsWithChildren) => {
-  const cacheStore = useState(new CacheStore())[0]
-  const queryClient = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            retry: 0,
-          },
-        },
-      })
-  )[0]
-  const suspensive = useState(
-    () =>
-      new Suspensive({
-        defaultProps: {
-          delay: {
-            ms: 1200,
-          },
-          suspense: {
-            fallback: <Spinner />,
-          },
-        },
-      })
-  )[0]
+  const cacheStore = useState(() => new CacheStore())[0]
+  const queryClient = useState(() => new QueryClient({ defaultOptions: { queries: { retry: 0 } } }))[0]
+  const defaultProps = useState(() => new DefaultProps({ delay: { ms: 1200 }, suspense: { fallback: <Spinner /> } }))[0]
 
   return (
     <CacheStoreProvider store={cacheStore}>
-      <SuspensiveProvider value={suspensive}>
+      <DefaultPropsProvider defaultProps={defaultProps}>
         <QueryClientProvider client={queryClient}>
           {children}
           <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
-        <DevMode position="bottomRight" />
-      </SuspensiveProvider>
+      </DefaultPropsProvider>
     </CacheStoreProvider>
   )
 }
