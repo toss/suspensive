8423226a16239eeb12226637065b39b9f78ac909
Jonghyeon Ko <jonghyeon@toss.im>
2024-08-02 13:01:07 +0900

    chore: update

--- packages/react-native/jest.config.ts
+++ packages/react-native/jest.config.ts
@@ -6,8 +6,6 @@ module.exports = {
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
-  transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|react-native-gesture-handler|react-native-reanimated|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
-  ],
+  transformIgnorePatterns: ['../../node_modules/(?!@react-native|react-native)'],
   setupFilesAfterEnv: ['./jest.setup.ts'],
 } satisfies Config
