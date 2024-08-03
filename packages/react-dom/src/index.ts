bce77f96d985d71ec0fa5718cef41648a99c5af1
Jonghyeon Ko <jonghyeon@toss.im>
2024-08-03 18:11:10 +0900

    feat(react-dom): init @suspensive/react-dom (#1182)
    close #1177 
    
    # Overview
    
    <!--
        A clear and concise description of what this pr is about.
     -->
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/toss/suspensive/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.

---
+++ packages/react-dom/src/index.ts
@@ -0,0 +1,1 @@
+export { TestText } from './TestText'
--- packages/utils/vitest.setup.ts
+++ packages/utils/vitest.setup.ts
@@ -1,8 +1,7 @@
 import '@testing-library/jest-dom/vitest'
 import { cleanup } from '@testing-library/react'
-import { afterEach, vi } from 'vitest'
+import { afterEach } from 'vitest'
 
-vi.stubEnv('NODE_ENV', 'development')
 afterEach(() => {
   cleanup()
 })
