007f9097b312216a4ecc40fb07bb9ba277797ab8
gwansikk <Seorit98@gmail.com>
2024-08-11 00:03:26 +0900

    feat(jotai): add test cases

--- packages/jotai/src/AtomValue.test-d.tsx
+++ packages/jotai/src/AtomValue.test-d.tsx
@@ -1,11 +1,46 @@
 import { atom, useAtomValue } from 'jotai'
+import { loadable } from 'jotai/utils'
 import type { ReactNode } from 'react'
 import { AtomValue } from './AtomValue'
 
-const asyncAtom = atom(() => Promise.resolve('string'))
+const countAtom = atom(0)
+const readOnlyCountAtom = atom((get) => get(countAtom))
+const writeOnlyCountAtom = atom(null, (get, set) => set(countAtom, get(countAtom) + 1))
+const asyncAtom = atom(async () => Promise.resolve('string'))
+const asyncIncrementAtom = atom(null, async (get, set) => {
+  set(countAtom, get(countAtom) + 1)
+})
+const loadableAtom = loadable(asyncAtom)
 
 describe('<AtomValue/>', () => {
   it('type check', () => {
+    ;() => (
+      <AtomValue atom={countAtom}>
+        {(value) => {
+          const valueOfJotai = useAtomValue(countAtom)
+          expectTypeOf(value).toEqualTypeOf<typeof valueOfJotai>()
+          return <></>
+        }}
+      </AtomValue>
+    )
+    ;() => (
+      <AtomValue atom={readOnlyCountAtom}>
+        {(value) => {
+          const valueOfJotai = useAtomValue(readOnlyCountAtom)
+          expectTypeOf(value).toEqualTypeOf<typeof valueOfJotai>()
+          return <></>
+        }}
+      </AtomValue>
+    )
+    ;() => (
+      <AtomValue atom={writeOnlyCountAtom}>
+        {(value) => {
+          const valueOfJotai = useAtomValue(writeOnlyCountAtom)
+          expectTypeOf(value).toEqualTypeOf<typeof valueOfJotai>()
+          return <></>
+        }}
+      </AtomValue>
+    )
     ;() => (
       <AtomValue atom={asyncAtom}>
         {(value) => {
@@ -15,8 +50,36 @@ describe('<AtomValue/>', () => {
         }}
       </AtomValue>
     )
+    ;() => (
+      <AtomValue atom={asyncIncrementAtom}>
+        {(value) => {
+          const valueOfJotai = useAtomValue(asyncIncrementAtom)
+          expectTypeOf(value).toEqualTypeOf<typeof valueOfJotai>()
+          return <></>
+        }}
+      </AtomValue>
+    )
+    ;() => (
+      <AtomValue atom={loadableAtom}>
+        {(value) => {
+          const valueOfJotai = useAtomValue(loadableAtom)
+          expectTypeOf(value).toEqualTypeOf<typeof valueOfJotai>()
+          return <></>
+        }}
+      </AtomValue>
+    )
 
+    expectTypeOf(<AtomValue atom={countAtom}>{() => <></>}</AtomValue>).toEqualTypeOf<JSX.Element>()
+    expectTypeOf(<AtomValue atom={countAtom}>{() => <></>}</AtomValue>).not.toEqualTypeOf<ReactNode>()
+    expectTypeOf(<AtomValue atom={readOnlyCountAtom}>{() => <></>}</AtomValue>).toEqualTypeOf<JSX.Element>()
+    expectTypeOf(<AtomValue atom={readOnlyCountAtom}>{() => <></>}</AtomValue>).not.toEqualTypeOf<ReactNode>()
+    expectTypeOf(<AtomValue atom={writeOnlyCountAtom}>{() => <></>}</AtomValue>).toEqualTypeOf<JSX.Element>()
+    expectTypeOf(<AtomValue atom={writeOnlyCountAtom}>{() => <></>}</AtomValue>).not.toEqualTypeOf<ReactNode>()
     expectTypeOf(<AtomValue atom={asyncAtom}>{() => <></>}</AtomValue>).toEqualTypeOf<JSX.Element>()
     expectTypeOf(<AtomValue atom={asyncAtom}>{() => <></>}</AtomValue>).not.toEqualTypeOf<ReactNode>()
+    expectTypeOf(<AtomValue atom={asyncIncrementAtom}>{() => <></>}</AtomValue>).toEqualTypeOf<JSX.Element>()
+    expectTypeOf(<AtomValue atom={asyncIncrementAtom}>{() => <></>}</AtomValue>).not.toEqualTypeOf<ReactNode>()
+    expectTypeOf(<AtomValue atom={loadableAtom}>{() => <></>}</AtomValue>).toEqualTypeOf<JSX.Element>()
+    expectTypeOf(<AtomValue atom={loadableAtom}>{() => <></>}</AtomValue>).not.toEqualTypeOf<ReactNode>()
   })
 })
--- packages/jotai/src/SetAtom.test-d.tsx
+++ packages/jotai/src/SetAtom.test-d.tsx
@@ -3,14 +3,36 @@ import type { ReactNode } from 'react'
 import { SetAtom } from './SetAtom'
 
 const countAtom = atom(0)
+const writeOnlyCountAtom = atom(null, (get, set) => set(countAtom, get(countAtom) + 1))
+const asyncIncrementAtom = atom(null, async (get, set) => {
+  set(countAtom, get(countAtom) + 1)
+})
 
 describe('<SetAtom/>', () => {
   it('type check', () => {
     ;() => (
       <SetAtom atom={countAtom}>
-        {(setCount) => {
-          const setCountOfJotai = useSetAtom(countAtom)
-          expectTypeOf(setCount).toEqualTypeOf<typeof setCountOfJotai>()
+        {(value) => {
+          const valueOfJotai = useSetAtom(countAtom)
+          expectTypeOf(value).toEqualTypeOf<typeof valueOfJotai>()
+          return <></>
+        }}
+      </SetAtom>
+    )
+    ;() => (
+      <SetAtom atom={writeOnlyCountAtom}>
+        {(value) => {
+          const valueOfJotai = useSetAtom(writeOnlyCountAtom)
+          expectTypeOf(value).toEqualTypeOf<typeof valueOfJotai>()
+          return <></>
+        }}
+      </SetAtom>
+    )
+    ;() => (
+      <SetAtom atom={asyncIncrementAtom}>
+        {(value) => {
+          const valueOfJotai = useSetAtom(asyncIncrementAtom)
+          expectTypeOf(value).toEqualTypeOf<typeof valueOfJotai>()
           return <></>
         }}
       </SetAtom>
@@ -18,5 +40,9 @@ describe('<SetAtom/>', () => {
 
     expectTypeOf(<SetAtom atom={countAtom}>{() => <></>}</SetAtom>).toEqualTypeOf<JSX.Element>()
     expectTypeOf(<SetAtom atom={countAtom}>{() => <></>}</SetAtom>).not.toEqualTypeOf<ReactNode>()
+    expectTypeOf(<SetAtom atom={writeOnlyCountAtom}>{() => <></>}</SetAtom>).toEqualTypeOf<JSX.Element>()
+    expectTypeOf(<SetAtom atom={writeOnlyCountAtom}>{() => <></>}</SetAtom>).not.toEqualTypeOf<ReactNode>()
+    expectTypeOf(<SetAtom atom={asyncIncrementAtom}>{() => <></>}</SetAtom>).toEqualTypeOf<JSX.Element>()
+    expectTypeOf(<SetAtom atom={asyncIncrementAtom}>{() => <></>}</SetAtom>).not.toEqualTypeOf<ReactNode>()
   })
 })
