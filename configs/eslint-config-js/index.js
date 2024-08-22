d049bc12d3282cda2d4cf7133d6dc1015cc3de60
vr-varad <varadgupta21#gmail.com>
2024-08-22 20:38:35 +0530

    feat(lint): introducing flat-config into eslint-config-js

--- configs/eslint-config-js/index.js
+++ configs/eslint-config-js/index.js
@@ -1,18 +1,25 @@
+import importPlugin from 'eslint-plugin-import'
+import noImportConfig from './no-import.js'
+
 /** @type {import('eslint').Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ['plugin:import/recommended', './no-import.js'],
-  plugins: ['import'],
-  rules: {
-    'sort-imports': ['error', { ignoreDeclarationSort: true }],
-    'import/no-cycle': 'error',
-    'import/no-duplicates': ['error', { 'prefer-inline': true }],
-    'import/order': [
-      'warn',
-      {
-        groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'index'],
-        alphabetize: { order: 'asc', caseInsensitive: true },
-      },
-    ],
+
+export default [
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      'sort-imports': ['error', { ignoreDeclarationSort: true }],
+      'import/no-cycle': 'error',
+      'import/no-duplicates': ['error', { 'prefer-inline': true }],
+      'import/order': [
+        'warn',
+        {
+          groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'index'],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+    },
   },
-}
+  ...noImportConfig,
+]
--- configs/eslint-config-js/no-import.js
+++ configs/eslint-config-js/no-import.js
@@ -1,47 +1,58 @@
+import cspellPlugin from '@cspell/eslint-plugin'
+import vitestPlugin from '@vitest/eslint-plugin'
+import jestDomPlugin from 'eslint-plugin-jest-dom'
+import jsdocPlugin from 'eslint-plugin-jsdoc'
+import prettierPlugin from 'eslint-plugin-prettier'
+
 /** @type {import('eslint').Linter.Config} */
-module.exports = {
-  root: true,
-  env: {
-    es2020: true,
-    browser: true,
-    node: true,
-    jest: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  plugins: ['jsdoc'],
-  extends: [
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-    'plugin:jsdoc/recommended',
-    'plugin:@cspell/recommended',
-  ],
-  rules: {
-    'no-warning-comments': 'warn',
-    'jsdoc/require-description': 'warn',
-    'jsdoc/require-returns': 'off',
-    'jsdoc/require-jsdoc': 'off',
-    'jsdoc/check-param-names': 'error',
-    '@cspell/spellchecker': [
-      'warn',
-      {
-        cspell: {
-          words: ['packlint', 'codecov', 'tsup', 'nextra', 'Sandpack'],
-        },
-      },
-    ],
-  },
-  overrides: [
-    {
-      files: ['*.spec.ts*', '*.test.ts*', '*.test-d.ts*'],
-      plugins: ['@vitest', 'jest-dom'],
-      extends: ['plugin:@vitest/legacy-recommended'],
-      settings: {
-        vitest: {
-          typecheck: true,
-        },
+
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: {
+        es2020: true,
+        browser: true,
+        node: true,
+        jest: true,
       },
     },
-  ],
-}
+    plugins: {
+      jsdoc: jsdocPlugin,
+      '@cspell': cspellPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'no-warning-comments': 'warn',
+      'jsdoc/require-description': 'warn',
+      'jsdoc/require-returns': 'off',
+      'jsdoc/require-jsdoc': 'off',
+      'jsdoc/check-param-names': 'error',
+      '@cspell/spellchecker': [
+        'warn',
+        {
+          cspell: {
+            words: ['packlint', 'codecov', 'tsup', 'nextra', 'Sandpack'],
+          },
+        },
+      ],
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+  },
+  {
+    files: ['*.spec.ts*', '*.test.ts*', '*.test-d.ts*'],
+    plugins: {
+      '@vitest': vitestPlugin,
+      'jest-dom': jestDomPlugin,
+    },
+    rules: {},
+    settings: {
+      vitest: {
+        typecheck: true,
+      },
+    },
+  },
+]
