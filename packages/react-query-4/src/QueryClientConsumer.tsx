5925504163dfb2c5daa5ebcbc71cc5889d74a82f
gwansikk <gwansik.kim@gwansik.dev>
2024-09-12 18:17:46 +0900

    feat: add jsdoc to QueryClientConsumer

--- packages/react-query-4/src/QueryClientConsumer.tsx
+++ packages/react-query-4/src/QueryClientConsumer.tsx
@@ -1,12 +1,15 @@
 import { type QueryClient, useQueryClient } from '@tanstack/react-query'
 import type { ReactNode } from 'react'
 
+/**
+ * @experimental This is experimental feature.
+ */
 export function QueryClientConsumer({
   children,
   context,
-}: Readonly<{
+}: {
   children: (queryClient: QueryClient) => ReactNode
   context?: React.Context<QueryClient | undefined>
-}>) {
+}) {
   return <>{children(useQueryClient({ context }))}</>
 }
--- packages/react-query-5/src/QueryClientConsumer.tsx
+++ packages/react-query-5/src/QueryClientConsumer.tsx
@@ -1,12 +1,15 @@
 import { type QueryClient, useQueryClient } from '@tanstack/react-query'
 import type { ReactNode } from 'react'
 
+/**
+ * @experimental This is experimental feature.
+ */
 export function QueryClientConsumer({
   children,
   queryClient,
-}: Readonly<{
+}: {
   children: (queryClient: QueryClient) => ReactNode
   queryClient?: QueryClient
-}>) {
+}) {
   return <>{children(useQueryClient(queryClient))}</>
 }
