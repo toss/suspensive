6fd0a0847a50ed55948d48cd1dc1fb514d722d82
Jonghyeon Ko <jonghyeon@toss.im>
2024-09-29 19:40:59 +0900

    chore(react-query-next-experimental): remove package (#1281)

--- examples/react-query-next-experimental-4-example/src/app/components/Text2.tsx
+++
@@ -1,8 +0,0 @@
-import { type ComponentProps, forwardRef } from 'react'
-
-export const Text2 = forwardRef<HTMLParagraphElement, ComponentProps<'p'>>((props, ref) => (
-  <p {...props} ref={ref}>
-    result: {props.children}
-  </p>
-))
-Text2.displayName = 'Text2'
--- packages/react-query-next-experimental-4/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { ReactQueryStreamedHydration } from './ReactQueryStreamedHydration'
