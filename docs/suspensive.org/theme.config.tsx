51b2308961d9c6ba4276d8be4429ec7151bc0808
Jonghyeon Ko <jonghyeon@toss.im>
2024-10-11 18:49:35 +0900

    docs(suspensive.org): nextra 3 (#1301)
    # Overview
    
    <!--
        A clear and concise description of what this pr is about.
     -->
    
    1. Nextra v2 -> v3 done!
    2. Some framer-motion is added
    
    
    ![chrome-capture-2024-10-11](https://github.com/user-attachments/assets/aa7f1dd3-1ab7-4328-98fc-44014fd3f8b9)
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/toss/suspensive/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.
    
    ---------
    
    Co-authored-by: Gwansik Kim <Seorit98@gmail.com>

--- docs/suspensive.org/theme.config.tsx
+++ docs/suspensive.org/theme.config.tsx
@@ -1,17 +1,19 @@
+import { motion } from 'framer-motion'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
+import { useRouter } from 'nextra/hooks'
 import { type DocsThemeConfig, useConfig } from 'nextra-theme-docs'
+import './src/styles/globals.css'
 
 const localeBanner = {
   en: (
     <span>
-      👀 Check out the changes in Suspensive v2. <Link href="/docs/migrate-to-v2">read more →</Link>
+      👀 Check out the changes in Suspensive v2. <Link href="/en/docs/migrate-to-v2">read more →</Link>
     </span>
   ),
   ko: (
     <span>
-      👀 Suspensive v2에서의 변경을 확인하세요. <Link href="/docs/migrate-to-v2">더보기 →</Link>
+      👀 Suspensive v2에서의 변경을 확인하세요. <Link href="/ko/docs/migrate-to-v2">더보기 →</Link>
     </span>
   ),
 } as const
@@ -19,7 +21,7 @@ const localeBanner = {
 const config: DocsThemeConfig = {
   banner: {
     key: 'suspensive banner',
-    text: function Text() {
+    content: function Text() {
       const { locale } = useRouter()
       return localeBanner[locale as keyof typeof localeBanner]
     },
@@ -30,13 +32,15 @@ const config: DocsThemeConfig = {
   },
   logo: function Logo() {
     return (
-      <div className="flex items-center gap-1">
-        <Image src="/img/logo_dark.png" width={34} height={34} alt="suspensive logo" />
+      <motion.div className="flex items-center gap-1" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+        <motion.span>
+          <Image src="/img/logo_dark.png" width={34} height={34} alt="suspensive logo" />
+        </motion.span>
         <div className="relative">
           <strong>Suspensive</strong>
           <span className="absolute text-[8px]">v2</span>
         </div>
-      </div>
+      </motion.div>
     )
   },
   head: function Head() {
@@ -59,20 +63,12 @@ const config: DocsThemeConfig = {
     link: 'https://github.com/toss/suspensive',
   },
   docsRepositoryBase: 'https://github.com/toss/suspensive/tree/main/docs/suspensive.org',
-  useNextSeoProps() {
-    const { asPath } = useRouter()
-    if (asPath !== '/') {
-      return {
-        titleTemplate: '%s – Suspensive',
-      }
-    }
-  },
   feedback: { content: '' },
   editLink: {
-    text: function Text() {
-      const router = useRouter()
+    content: function Text() {
+      const { locale } = useRouter()
 
-      if (router.pathname.includes('.ko')) {
+      if (locale === 'ko') {
         return <>이 페이지를 수정하기 →</>
       }
 
@@ -80,21 +76,18 @@ const config: DocsThemeConfig = {
     },
   },
   sidebar: {
-    titleComponent({ title }) {
-      return <>{title}</>
-    },
     defaultMenuCollapseLevel: 4,
     toggleButton: true,
   },
   i18n: [
-    { locale: 'en', text: 'English' },
-    { locale: 'ko', text: '한국어' },
+    { locale: 'en', name: 'English' },
+    { locale: 'ko', name: '한국어' },
   ],
   search: {
     placeholder: function Placeholder() {
       const router = useRouter()
 
-      if (router.pathname.includes('.ko')) {
+      if (router.locale === 'ko') {
         return '검색어를 입력하세요...'
       }
 
@@ -102,11 +95,25 @@ const config: DocsThemeConfig = {
     },
   },
   footer: {
-    text: 'MIT 2024 © Viva Republica, Inc.',
+    content: 'MIT 2024 © Viva Republica, Inc.',
   },
   darkMode: false,
   nextThemes: {
     forcedTheme: 'dark',
+  },
+  toc: {
+    backToTop: true,
+    float: true,
+  },
+  navigation: true,
+  main: ({ children }) => {
+    const router = useRouter()
+
+    return (
+      <motion.div key={router.asPath} initial={{ opacity: 0, y: 8 }} animate={{ opacity: 1, y: 0 }}>
+        {children}
+      </motion.div>
+    )
   },
 }
 
