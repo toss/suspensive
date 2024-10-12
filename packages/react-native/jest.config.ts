4fabbc00b1f2307d47d211c5b073e718677fbe97
gwansikk <gwansik.kim@gwansik.dev>
2024-10-12 19:49:50 +0900

    fix(react-native): fix jest error in react native

--- packages/react-native/jest.config.ts
+++ packages/react-native/jest.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from 'jest'
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-module.exports = {
+const config: Config = {
   preset: 'jest-expo',
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   transformIgnorePatterns: ['../../node_modules/(?!@react-native|react-native)'],
   setupFilesAfterEnv: ['./jest.setup.ts'],
-} satisfies Config
+}
+
+export default config
