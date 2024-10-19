f6e3cda3c21c2c70638df50b3e4cb8466dde2652
dayongkr <dayongkr@gmail.com>
2024-10-19 14:02:19 +0900

    refactor(react): update `usePreservedCallback` to use `useEffect` instead of `useLayoutEffect`

--- packages/react/src/hooks/usePreservedCallback.ts
+++ packages/react/src/hooks/usePreservedCallback.ts
@@ -1,14 +1,13 @@
-import { useCallback, useRef } from 'react'
-import { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect'
+import { useCallback, useEffect, useRef } from 'react'
 
-export const usePreservedCallback = <T extends (...args: any[]) => unknown>(callback: T): T => {
+export const usePreservedCallback = <T extends (...args: any[]) => unknown>(callback: T) => {
   const callbackRef = useRef<T>(callback)
 
-  useIsomorphicLayoutEffect(() => {
+  useEffect(() => {
     callbackRef.current = callback
   }, [callback])
 
   return useCallback((...args: Parameters<T>) => {
     return callbackRef.current(...args)
-  }, []) as T
+  }, [])
 }
