91d8ae1d63fee54f147bca9fc54bcb086d4061cf
Dayong Lee <dayongkr@gmail.com>
2024-10-26 22:39:56 +0900

    fix(react): change `defineSuspense` function signature (#1336)
    # Overview
    
    I made modifications because, when `componentPropsClientOnly` is set to
    `false`, it returns `Suspense`, but the overloaded function signature
    indicated that it returns `SuspenseClientOnly`.
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/toss/suspensive/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.
    
    ---------
    
    Co-authored-by: Jonghyeon Ko <jonghyeon@toss.im>

---
+++ .changeset/eleven-laws-appear.md
@@ -0,0 +1,5 @@
+---
+"@suspensive/react": patch
+---
+
+fix(react): change `defineSuspense` function signature
--- packages/react/src/utils/defineSuspense.test-d.ts
+++ packages/react/src/utils/defineSuspense.test-d.ts
@@ -5,20 +5,20 @@ describe('defineSuspense', () => {
   it('type check', () => {
     expectTypeOf(defineSuspense({ componentPropsClientOnly: true })).toEqualTypeOf<typeof SuspenseClientOnly>()
     expectTypeOf(defineSuspense({ defaultPropsClientOnly: true })).toEqualTypeOf<typeof SuspenseClientOnly>()
-    expectTypeOf(defineSuspense({ componentPropsClientOnly: true, defaultPropsClientOnly: undefined })).toEqualTypeOf<
+    expectTypeOf(defineSuspense({ defaultPropsClientOnly: undefined, componentPropsClientOnly: true })).toEqualTypeOf<
       typeof SuspenseClientOnly
     >()
-    expectTypeOf(defineSuspense({ componentPropsClientOnly: undefined, defaultPropsClientOnly: true })).toEqualTypeOf<
+    expectTypeOf(defineSuspense({ defaultPropsClientOnly: true, componentPropsClientOnly: undefined })).toEqualTypeOf<
       typeof SuspenseClientOnly
     >()
     expectTypeOf(defineSuspense({ defaultPropsClientOnly: true, componentPropsClientOnly: true })).toEqualTypeOf<
       typeof SuspenseClientOnly
     >()
-    expectTypeOf(defineSuspense({ defaultPropsClientOnly: true, componentPropsClientOnly: false })).toEqualTypeOf<
-      typeof SuspenseClientOnly
-    >()
     expectTypeOf(defineSuspense({ defaultPropsClientOnly: false, componentPropsClientOnly: true })).toEqualTypeOf<
       typeof SuspenseClientOnly
+    >()
+    expectTypeOf(defineSuspense({ defaultPropsClientOnly: true, componentPropsClientOnly: false })).toEqualTypeOf<
+      typeof Suspense
     >()
     expectTypeOf(defineSuspense({ defaultPropsClientOnly: false, componentPropsClientOnly: false })).toEqualTypeOf<
       typeof Suspense
