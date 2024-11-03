35b842a3ba0f01b6eabcead25fe2093e0ea39820
Jonghyeon Ko <jonghyeon@toss.im>
2024-11-03 16:56:14 +0900

    chore: update

--- examples/visualization/src/app/react-query/SuspenseQuery/page.tsx
+++ examples/visualization/src/app/react-query/SuspenseQuery/page.tsx
@@ -1,46 +1,50 @@
 'use client'
 
 import { ErrorBoundary, Suspense } from '@suspensive/react'
-import { SuspenseQuery } from '@suspensive/react-query'
+import { SuspenseQuery, queryOptions, usePrefetchQuery } from '@suspensive/react-query'
 import axios from 'axios'
 import { Spinner } from '~/components/uis'
 
+const query = {
+  user: (userId: number) =>
+    queryOptions({
+      queryKey: ['users', userId],
+      queryFn: () =>
+        axios
+          .get<{
+            id: number
+            username: string
+            age: number
+          }>(`https://dummyjson.com/users/${userId}`)
+          .then(({ data }) => data),
+    }),
+  products: () =>
+    queryOptions({
+      queryKey: ['products'],
+      queryFn: () =>
+        axios
+          .get<{
+            products: Array<{
+              id: number
+              title: string
+              price: number
+            }>
+          }>(`https://dummyjson.com/products`)
+          .then(({ data }) => data),
+    }),
+}
+
 export default function Page() {
   const userId = 1
+  usePrefetchQuery(query.user(userId))
+  usePrefetchQuery(query.products())
 
   return (
     <ErrorBoundary fallback={({ error }) => <>{error.message}</>}>
-      <Suspense clientOnly fallback={<Spinner />}>
+      <Suspense clientOnly fallback={<ExpensiveSpinner ms={2000} />}>
         <div>
-          <SuspenseQuery
-            queryKey={['users', userId]}
-            queryFn={() =>
-              axios
-                .get<{
-                  id: number
-                  username: string
-                  age: number
-                }>(`https://dummyjson.com/users/${userId}`)
-                .then(({ data }) => data)
-            }
-          >
-            {({ data: user }) => <h1>{user.username}</h1>}
-          </SuspenseQuery>
-          <SuspenseQuery
-            queryKey={['products']}
-            queryFn={() =>
-              axios
-                .get<{
-                  products: Array<{
-                    id: number
-                    title: string
-                    price: number
-                  }>
-                }>(`https://dummyjson.com/products`)
-                .then(({ data }) => data)
-            }
-            select={(data) => data.products}
-          >
+          <SuspenseQuery {...query.user(userId)}>{({ data: user }) => <h1>{user.username}</h1>}</SuspenseQuery>
+          <SuspenseQuery {...query.products()} select={(data) => data.products}>
             {({ data: products }) => (
               <div>
                 {products.map((product) => (
@@ -53,4 +57,12 @@ export default function Page() {
       </Suspense>
     </ErrorBoundary>
   )
+}
+
+const ExpensiveSpinner = ({ ms }: { ms: number }) => {
+  if (typeof window !== 'undefined') {
+    const start = Date.now()
+    while (Date.now() - start < ms) {}
+  }
+  return <Spinner />
 }
