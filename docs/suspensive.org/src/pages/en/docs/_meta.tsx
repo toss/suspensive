766a674679f6b431afa2de1e329b137399942d27
gwansikk <gwansik.kim@gwansik.dev>
2024-11-07 15:30:18 +0900

    feat: update docs

--- docs/suspensive.org/src/pages/en/docs/_meta.tsx
+++ docs/suspensive.org/src/pages/en/docs/_meta.tsx
@@ -3,15 +3,10 @@ export default {
     type: 'separator',
     title: 'Packages',
   },
-  react: {
-    title: '@suspensive/react',
-  },
-  'react-query': {
-    title: '@suspensive/react-query',
-  },
-  jotai: {
-    title: '@suspensive/jotai',
-  },
+  react: { title: '@suspensive/react' },
+  'react-query': { title: '@suspensive/react-query' },
+  jotai: { title: '@suspensive/jotai' },
+  codemods: { title: '@suspensive/codemods' },
   '--- More': {
     type: 'separator',
     title: 'More',
---
+++ docs/suspensive.org/src/pages/en/docs/codemods/_meta.tsx
@@ -0,0 +1,9 @@
+export default {
+  motivation: { title: 'Why need to use?' },
+  usage: { title: 'Usage' },
+  '--- Codemods': {
+    type: 'separator',
+    title: 'Codemods',
+  },
+  tanstackQueryImport: { title: 'Migrate TanStack Query imports' },
+}
