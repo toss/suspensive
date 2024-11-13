211c0600e404cd411c4dc76a2cc15ee4ed2b84bb
YuGilJong <101111364+XionWCFM@users.noreply.github.com>
2024-11-14 04:07:57 +0900

    fix: React Native Playground Configuration (#1352)
    # Overview
    
    Configure an environment to run React Native Playground.
    
    ## Guide
    
    
    1. install dependencies 
    
    ```
    pnpm i
    ```
    
    2. run project
    
    ```
    cd examples/react-native-playground
    pnpm run start
    i
    ```
    
    
    3. The following screen will be displayed:
    
    
    <img width="357" alt="Screenshot 2024-11-13 at 18 08 25"
    src="https://github.com/user-attachments/assets/a71a59c1-980a-46f2-898c-764c277960c2">
    
    <img width="1256" alt="Screenshot 2024-11-13 at 18 12 07"
    src="https://github.com/user-attachments/assets/59828f3a-fa87-4f24-b55d-0855771217c4">
    
    
    
    ## Test
    
    built the entire project and confirmed that it was built without a
    problem.
    
    
    ## Trouble Shooting
    
    React Native is sensitive to cache and nodemodule state.
    
    If it doesn't work well, delete the entire nodemodules and cache folders
    in the project and try again.
    
    
    ## ETC
    
    
    I realized that public-hoist-pattern[]=* is not the same as
    node-linker=hoisted, but rather the same as setting shamefully-hoist to
    true.
    
    I set node-linker=hoisted in another project and experienced next15's
    build failing during the CI process.
    
    
    
    By default, pnpm manages dependencies strictly so that only specified
    dependencies are used.
    
    However, if you set this setting, you will give up strict dependency
    management and manage dependencies similar to the method of older
    generation package managers such as yarn and npm.
    
    Should we give up the functionality of pnpm? I wish there was another
    way to solve this problem.
    
    I think I can use it until I find another way. shamefully-hoist and
    public-hoist-pattern[]=*
    
    ---------
    
    Co-authored-by: Jonghyeon Ko <manudeli.ko@gmail.com>

--- examples/react-native-playground/babel.config.js
+++ examples/react-native-playground/babel.config.js
@@ -1,6 +1,19 @@
-module.exports = function (api) {
-  api.cache(true);
+module.exports = (api) => {
+  api.cache(true)
   return {
-    presets: ["babel-preset-expo"],
-  };
-};
+    presets: [['babel-preset-expo']],
+
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+
+          alias: {
+            '@': './',
+          },
+        },
+      ],
+    ],
+  }
+}
--- examples/react-native-playground/metro.config.js
+++ examples/react-native-playground/metro.config.js
@@ -1,21 +1,24 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require("expo/metro-config");
-const path = require("path");
+const { getDefaultConfig } = require('expo/metro-config')
+const { withNativeWind } = require('nativewind/metro')
+const { FileStore } = require('metro-cache')
+const path = require('node:path')
 
-// Find the workspace root, this can be replaced with `find-yarn-workspace-root`
-const workspaceRoot = path.resolve(__dirname, "../..");
-const projectRoot = __dirname;
+const projectRoot = __dirname
+const workspaceRoot = path.resolve(projectRoot, '../..')
 
-const config = getDefaultConfig(projectRoot);
+const config = getDefaultConfig(projectRoot)
 
-// 1. Watch all files within the monorepo
-config.watchFolders = [workspaceRoot];
-// 2. Let Metro know where to resolve packages, and in what order
+config.watchFolders = [workspaceRoot]
+config.resolver.disableHierarchicalLookup = true
 config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, "node_modules"),
-  path.resolve(workspaceRoot, "node_modules"),
-];
-// 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
-config.resolver.disableHierarchicalLookup = true;
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+]
 
-module.exports = config;
+config.cacheStores = [
+  new FileStore({
+    root: path.join(projectRoot, 'node_modules', '.cache', 'metro'),
+  }),
+]
+
+module.exports = config
