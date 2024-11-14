912937ef44eafeb21b52fd70cb376f81982688be
Jonghyeon Ko <jonghyeon@toss.im>
2024-11-15 03:56:11 +0900

    test(react-query-4): type test for usePrefetchQuery, usePrefetchInfiniteQuery (#1359)
    close #1298
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/toss/suspensive/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.

---
+++ packages/react-query-4/src/usePrefetchInfiniteQuery.test-d.tsx
@@ -0,0 +1,14 @@
+import { infiniteQueryOptions } from './infiniteQueryOptions'
+import { queryFn, queryKey } from './test-utils'
+import { usePrefetchInfiniteQuery } from './usePrefetchInfiniteQuery'
+
+describe('usePrefetchInfiniteQuery', () => {
+  it('type check', () => {
+    expectTypeOf(usePrefetchInfiniteQuery({ queryFn, queryKey }))
+      // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+      .toEqualTypeOf<void>()
+    expectTypeOf(usePrefetchInfiniteQuery(infiniteQueryOptions({ queryFn, queryKey })))
+      // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+      .toEqualTypeOf<void>()
+  })
+})
