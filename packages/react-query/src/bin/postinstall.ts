a7435177c5a43912d30b58929785cfb3524a940d
gwansikk <gwansik.kim@gwansik.dev>
2024-11-23 16:21:11 +0900

    refactor: remove try-catch postinstall

--- packages/react-query/src/bin/postinstall.ts
+++ packages/react-query/src/bin/postinstall.ts
@@ -1,22 +1,12 @@
 import { getTanStackReactQueryPackageJson } from './utils/package'
 import { switchVersion } from './utils/switchVersion'
 
-try {
-  const { version } = getTanStackReactQueryPackageJson()
+const { version } = getTanStackReactQueryPackageJson()
 
-  if (!version) {
-    throw new Error()
-  }
-
-  if (version.startsWith('4.')) {
-    switchVersion(4)
-  } else if (version.startsWith('5.')) {
-    switchVersion(5)
-  } else {
-    console.warn('[@suspensive/react-query]', `version v${version} is not supported.`)
-  }
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-} catch (error) {
-  console.error('[@suspensive/react-query]', 'not fond @tanstack/react-query')
-  process.exit(1)
+if (version.startsWith('4.')) {
+  switchVersion(4)
+} else if (version.startsWith('5.')) {
+  switchVersion(5)
+} else {
+  console.warn('[@suspensive/react-query]', `version v${version} is not supported.`)
 }
