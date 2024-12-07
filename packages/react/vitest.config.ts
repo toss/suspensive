cc757a84d2196511f7beb1a1363c7342afc8c134
Jonghyeon Ko <jonghyeon@toss.im>
2024-12-07 19:57:06 +0900

    chore(*): update deps

--- packages/react/vitest.config.ts
+++ packages/react/vitest.config.ts
@@ -1,16 +1,13 @@
-import codspeedPlugin from '@codspeed/vitest-plugin'
 import { defineConfig } from 'vitest/config'
 import packageJson from './package.json'
 
 export default defineConfig({
-  plugins: process.env.CI === 'true' ? [codspeedPlugin()] : [],
   test: {
     name: packageJson.name,
     dir: './src',
     environment: 'jsdom',
     globals: true,
     setupFiles: './vitest.setup.ts',
-    benchmark: { outputJson: `benchmarks/${packageJson.version}.json` },
     coverage: {
       provider: 'istanbul',
     },
