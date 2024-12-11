661f97eadf9e1ead48fcb7b05c75c32ac155e686
Jonghyeon Ko <jonghyeon@toss.im>
2024-12-12 01:00:40 +0900

    chore: update

--- examples/vite-react-18-suspense-prerender-siblings-problem/src/main.tsx
+++ examples/vite-react-18-suspense-prerender-siblings-problem/src/main.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
@@ -19,7 +18,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={client}>
       <App />
-      <ReactQueryDevtools client={client} />
     </QueryClientProvider>
   </StrictMode>
 )
