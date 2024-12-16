a8f476c6d27086eaf4383b104bbd429ff5588b02
Jonghyeon Ko <jonghyeon@toss.im>
2024-12-17 03:50:55 +0900

    docs(suspensive.org): update font size for SectionText

--- docs/suspensive.org/src/components/SectionText.tsx
+++ docs/suspensive.org/src/components/SectionText.tsx
@@ -1,10 +1,10 @@
 import type { ReactNode } from 'react'
 
 export const SectionTitle = ({ children }: { children: ReactNode }) => (
-  <h3 className="text-1xl text-center font-bold opacity-60 md:text-2xl">
+  <h3 className="text-md text-center font-bold opacity-60 md:text-2xl">
     {children}
   </h3>
 )
 export const SectionDescription = ({ children }: { children: ReactNode }) => (
-  <h4 className="text-center text-3xl font-bold md:text-4xl">{children}</h4>
+  <h4 className="text-center text-2xl font-bold md:text-4xl">{children}</h4>
 )
