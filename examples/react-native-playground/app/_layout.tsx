d963530149d10828c1f5235a44687d3f82d7cbc6
Jonghyeon Ko <jonghyeon@toss.im>
2025-01-12 18:13:35 +0900

    chore: update

--- examples/react-native-playground/app/_layout.tsx
+++ examples/react-native-playground/app/_layout.tsx
@@ -1,16 +1,6 @@
 import { Slot } from 'expo-router'
-import * as SplashScreen from 'expo-splash-screen'
-import { StatusBar } from 'expo-status-bar'
-import { Providers } from '@/src/providers'
-import 'react-native-reanimated'
-
-SplashScreen.preventAutoHideAsync()
+import 'react'
 
 export default function RootLayout() {
-  return (
-    <Providers>
-      <StatusBar style="auto" />
-      <Slot />
-    </Providers>
-  )
+  return <Slot />
 }
