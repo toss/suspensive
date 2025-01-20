40f27842c230fcb90fcb16d13962f687978f0818
gwansikk <gwansik.kim@gwansik.dev>
2025-01-21 01:19:43 +0900

    feat: add copy button to landing page

--- docs/suspensive.org/src/components/HomePage.tsx
+++ docs/suspensive.org/src/components/HomePage.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'nextra/hooks'
 import { useEffect, useRef } from 'react'
+import { CopyButton } from './CopyButton'
 
 const CodeBlockClassName = 'nextra-code'
 
@@ -16,6 +17,7 @@ const backtickToCodeBlock = (text: string) =>
 const formatCodeBlocks = (desc: string) => backtickToCodeBlock(escapeHtml(desc))
 
 export const HomePage = ({
+  title,
   description,
   buttonText,
   items,
@@ -60,8 +62,8 @@ export const HomePage = ({
               />
             </div>
             <div className="flex flex-col items-center gap-4">
-              <div className="break-keep px-4 text-4xl font-bold leading-tight md:text-6xl">
-                <span>{description}</span>
+              <div className="break-keep bg-gradient-to-r from-white via-white/80 to-white/30 bg-clip-text px-4 text-4xl font-bold leading-tight text-transparent md:text-6xl">
+                <span>{title}</span>
               </div>
               <motion.p
                 initial={{ opacity: 0 }}
@@ -71,32 +73,35 @@ export const HomePage = ({
                 }}
                 className="rounded-full text-lg text-white md:text-xl"
               >
-                npm i @suspensive/react
+                {description}
               </motion.p>
             </div>
           </div>
-          <Link href={`/${router.locale}/docs/react/motivation`}>
-            <motion.button
-              whileHover={{
-                scale: 1.05,
-              }}
-              whileTap={{ scale: 0.95 }}
-              initial={{ opacity: 0 }}
-              animate={{
-                opacity: 1,
-                transition: {
-                  delay: 0.8,
-                  duration: 2,
-                },
-                filter:
-                  'drop-shadow(0 0 1px rgba(255, 255, 255, 0.5)) drop-shadow(0 0 3px rgba(255, 255, 255, 0.5)) drop-shadow(0 0 10px rgba(255, 255, 255, 0.5))',
-              }}
-              type="button"
-              className="rounded-xl bg-white px-8 py-3 text-lg font-bold text-[#111111] md:px-10 md:py-3 md:text-xl"
-            >
-              {buttonText}
-            </motion.button>
-          </Link>
+          <div className="flex items-center gap-4">
+            <CopyButton>npm i @suspensive/react</CopyButton>
+            <Link href={`/${router.locale}/docs/react/motivation`}>
+              <motion.button
+                whileHover={{
+                  scale: 1.05,
+                }}
+                whileTap={{ scale: 0.95 }}
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: 1,
+                  transition: {
+                    delay: 0.8,
+                    duration: 2,
+                  },
+                  filter:
+                    'drop-shadow(0 0 1px rgba(255, 255, 255, 0.5)) drop-shadow(0 0 3px rgba(255, 255, 255, 0.5)) drop-shadow(0 0 10px rgba(255, 255, 255, 0.5))',
+                }}
+                type="button"
+                className="rounded-xl bg-white px-7 py-2 text-lg font-bold text-[#111111] md:px-9 md:py-3 md:text-xl"
+              >
+                {buttonText}
+              </motion.button>
+            </Link>
+          </div>
         </div>
 
         <div className="h-14" />
