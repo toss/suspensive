6ee5b4eaa8ecb295446db591721bd8122b469224
Jonghyeon Ko <jonghyeon@toss.im>
2025-04-06 17:04:20 +0900

    docs(suspensive.org): add docs for InView (#1472)
    # Overview
    
    <!--
        A clear and concise description of what this pr is about.
     -->
    
    ![chrome-capture-2025-4-6
    (2)](https://github.com/user-attachments/assets/93373a3d-1487-4e3d-af5e-b84deb4c18b7)
    
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/toss/suspensive/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.

--- docs/suspensive.org/src/content/en/docs/_meta.tsx
+++ docs/suspensive.org/src/content/en/docs/_meta.tsx
@@ -6,6 +6,7 @@ export default {
     title: 'Packages',
   },
   react: { title: '@suspensive/react' },
+  'react-dom': { title: '@suspensive/react-dom' },
   'react-query': { title: '@suspensive/react-query' },
   jotai: { title: '@suspensive/jotai' },
   codemods: { title: '@suspensive/codemods' },
