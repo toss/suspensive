55d3e1a56526b5b4ade0472e491e088c63a24064
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-04-14 02:44:36 +0900

    refactor(react-query): replace if-else with switch statement for version handling

--- packages/react-query/src/bin/postinstall.ts
+++ packages/react-query/src/bin/postinstall.ts
@@ -4,10 +4,16 @@ import { switchVersion } from './utils/switchVersion'
 
 const { version } = getTanStackReactQueryPackageJson()
 
-if (version.startsWith('4.')) {
-  switchVersion(4)
-} else if (version.startsWith('5.')) {
-  switchVersion(5)
-} else {
-  logger.error(`version v${version} is not supported.`)
+const major = version.split('.')[0]
+
+switch (major) {
+  case '4':
+    switchVersion(4)
+    break
+  case '5':
+    switchVersion(5)
+    break
+  default:
+    logger.error(`version v${version} is not supported.`)
+    break
 }
