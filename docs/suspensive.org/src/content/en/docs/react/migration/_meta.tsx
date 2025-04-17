03fae04269867c685a4e70ae493616b5271e9525
Jonghyeon Ko <jonghyeon@toss.im>
2025-04-17 23:03:36 +0900

    docs(suspensive.org): update text

--- docs/suspensive.org/src/content/en/docs/react/migration/_meta.tsx
+++ docs/suspensive.org/src/content/en/docs/react/migration/_meta.tsx
@@ -2,5 +2,5 @@ import type { MetaRecord } from 'nextra'
 
 export default {
   'migrate-to-v3': { title: 'Migrate to v3' },
-  'migrate-to-v2': { title: 'Migrate to v3' },
+  'migrate-to-v2': { title: 'Migrate to v2' },
 } satisfies MetaRecord
