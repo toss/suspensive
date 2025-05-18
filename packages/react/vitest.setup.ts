c89fada09b3de7f5f90e942119f9d0801b62c7a1
Wonsuk Choi <sukvvon@gmail.com>
2025-05-18 23:02:24 +0900

    chore(vitest.setup): remove 'vitest' imports (#1585)

--- packages/react/vitest.setup.ts
+++ packages/react/vitest.setup.ts
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom/vitest'
 import { cleanup } from '@testing-library/react'
-import { afterEach, vi } from 'vitest'
 
 vi.stubEnv('NODE_ENV', 'development')
 afterEach(() => {
