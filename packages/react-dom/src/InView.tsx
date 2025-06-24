9ba127ee1e7e21b626e7dca8ad2ad56a0c3ee2dc
Wonsuk Choi <sukvvon@gmail.com>
2025-06-24 14:06:08 +0900

    feat(react-dom/InView): add JSDoc comments explaining IntersectionObserver usage (#1631)
    Co-authored-by: Jonghyeon Ko <jonghyeon@toss.im>

--- packages/react-dom/src/InView.tsx
+++ packages/react-dom/src/InView.tsx
@@ -4,6 +4,10 @@ interface InViewProps extends InViewOptions {
   children: (inViewResult: ReturnType<typeof useInView>) => React.ReactNode
 }
 
+/**
+ * This component uses the IntersectionObserver API to determine when its children enter the viewport.
+ * @see {@link https://suspensive.org/docs/react-dom/InView Suspensive Docs}
+ */
 export function InView({ children, ...options }: InViewProps) {
   return <>{children(useInView(options))}</>
 }
