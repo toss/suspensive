83984330786cb159a3b1ae6ed2399277aed6c67a
gwansikk <gwansik.kim@gwansik.dev>
2025-08-13 22:06:49 +0900

    chore(deps): update dependency package versions

--- packages/react-dom/vitest.workspace.ts
+++ packages/react-dom/vitest.workspace.ts
@@ -1,27 +1,31 @@
-import { defineWorkspace } from 'vitest/config'
+import { defineConfig } from 'vitest/config'
 
-export default defineWorkspace([
-  {
-    extends: 'vitest.config.ts',
-    test: {
-      include: ['**/*.{spec,test}.{ts,tsx}'],
-      exclude: ['browser/*.{spec,test}.{ts,tsx}'],
-      name: 'jsdom',
-      environment: 'jsdom',
-    },
-  },
-  {
-    extends: 'vitest.config.ts',
-    test: {
-      include: ['browser/*.{spec,test}.{ts,tsx}'],
-      name: 'browser',
-      environment: 'node',
-      browser: {
-        enabled: true,
-        headless: true,
-        provider: 'playwright',
-        name: 'chromium',
+export default defineConfig({
+  test: {
+    projects: [
+      {
+        extends: 'vitest.config.ts',
+        test: {
+          include: ['**/*.{spec,test}.{ts,tsx}'],
+          exclude: ['browser/*.{spec,test}.{ts,tsx}'],
+          name: 'jsdom',
+          environment: 'jsdom',
+        },
       },
-    },
+      {
+        extends: 'vitest.config.ts',
+        test: {
+          include: ['browser/*.{spec,test}.{ts,tsx}'],
+          name: 'browser',
+          environment: 'node',
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: 'playwright',
+            name: 'chromium',
+          },
+        },
+      },
+    ],
   },
-])
+})
