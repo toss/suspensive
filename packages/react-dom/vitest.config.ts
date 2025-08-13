970a5b05feda4f82056f1aa1db517006afc9c8bc
gwansikk <gwansik.kim@gwansik.dev>
2025-08-13 23:50:14 +0900

    chore: update

--- packages/react-dom/vitest.config.ts
+++ packages/react-dom/vitest.config.ts
@@ -10,25 +10,29 @@ export default defineConfig({
     typecheck: { enabled: true },
     projects: [
       {
-        extends: 'vitest.config.ts',
         test: {
-          include: ['**/*.{spec,test}.{ts,tsx}'],
-          exclude: ['browser/*.{spec,test}.{ts,tsx}'],
+          include: ['src/**/*.{spec,test}.{ts,tsx}'],
+          exclude: ['src/browser/**/*.{spec,test}.{ts,tsx}'],
           name: 'jsdom',
           environment: 'jsdom',
+          globals: true,
+          setupFiles: './vitest.setup.ts',
+          typecheck: { enabled: true },
         },
       },
       {
-        extends: 'vitest.config.ts',
         test: {
-          include: ['browser/*.{spec,test}.{ts,tsx}'],
+          include: ['src/browser/**/*.{spec,test}.{ts,tsx}'],
           name: 'browser',
           environment: 'node',
+          globals: true,
+          setupFiles: './vitest.setup.ts',
+          typecheck: { enabled: true },
           browser: {
             enabled: true,
             headless: true,
             provider: 'playwright',
-            name: 'chromium',
+            instances: [{ browser: 'chromium' }],
           },
         },
       },
