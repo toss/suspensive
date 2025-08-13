1c519c8a53f28b2a122595edb1aae2397d3119b1
gwansikk <gwansik.kim@gwansik.dev>
2025-08-13 23:30:41 +0900

    chore: update

--- packages/react-dom/vitest.config.ts
+++ packages/react-dom/vitest.config.ts
@@ -8,5 +8,30 @@ export default defineConfig({
     setupFiles: './vitest.setup.ts',
     coverage: { enabled: true, provider: 'istanbul' },
     typecheck: { enabled: true },
+    projects: [
+      {
+        extends: 'vitest.config.ts',
+        test: {
+          include: ['**/*.{spec,test}.{ts,tsx}'],
+          exclude: ['browser/*.{spec,test}.{ts,tsx}'],
+          name: 'jsdom',
+          environment: 'jsdom',
+        },
+      },
+      {
+        extends: 'vitest.config.ts',
+        test: {
+          include: ['browser/*.{spec,test}.{ts,tsx}'],
+          name: 'browser',
+          environment: 'node',
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: 'playwright',
+            name: 'chromium',
+          },
+        },
+      },
+    ],
   },
 })
