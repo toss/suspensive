ae7aaa6de0a807d6415ef3201232883c59c43d38
Marshall Ku <marshall@kakao.com>
2025-08-29 21:08:34 +0900

    docs(suspensive.org): Modify size of logo

--- docs/suspensive.org/src/components/Logo.tsx
+++ docs/suspensive.org/src/components/Logo.tsx
@@ -1,11 +1,20 @@
 import Image from 'next/image'
 
-export const LogoImage = ({ size = 1 }: { size: number }) => (
-  <Image
-    width={330 * size}
-    height={43.7 * size}
-    src="/img/logo-suspensive-text-progressive-blur.svg"
-    unoptimized
-    alt="Suspensive with star"
-  />
-)
+export const LogoImage = ({ size = 1 }: { size: number }) => {
+  const baseWidth = 330 * size
+  const baseHeight = 43.7 * size
+
+  return (
+    <Image
+      width={baseWidth}
+      height={baseHeight}
+      src="/img/logo-suspensive-text-progressive-blur.svg"
+      unoptimized
+      alt="Suspensive with star"
+      className="h-auto w-full"
+      style={{
+        width: `clamp(0px, ${baseWidth}px, calc(100% - 24px))`,
+      }}
+    />
+  )
+}
