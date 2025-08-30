25661bf7ae65a912726de370df734c5666265f74
Jonghyeon Ko <jonghyeon@toss.im>
2025-08-30 19:17:25 +0900

    chore: add prettier-plugin-sort-re-exports (#1718)
    Co-authored-by: Dayong Lee <50468628+dayongkr@users.noreply.github.com>

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -12,4 +12,5 @@ export default {
   singleQuote: true,
   tabWidth: 2,
   trailingComma: 'es5',
+  overrides: [{ files: 'packages/*/src/index.ts', options: { plugins: ['prettier-plugin-sort-re-exports'] } }],
 }
