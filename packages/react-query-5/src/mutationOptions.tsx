67d7bdb8d280563cbc5ffff41e2efa5175ab13ae
Jonghyeon Ko <jonghyeon@toss.im>
2025-09-04 23:45:42 +0900

    chore: update react-query and related packages to version 5.85.9
    - Updated versions of '@tanstack/react-query', '@tanstack/react-query-devtools', and '@tanstack/react-query-next-experimental' in pnpm-lock.yaml and pnpm-workspace.yaml.
    - Adjusted peer dependencies in package.json for compatibility with the new version.
    - Refactored mutationOptions and PrefetchQuery components to align with the latest API changes.
    - Added deprecation warnings for the old mutationOptions usage.

--- packages/react-query-5/src/mutationOptions.tsx
+++ packages/react-query-5/src/mutationOptions.tsx
@@ -1,10 +1,12 @@
-import type { DefaultError, UseMutationOptions } from '@tanstack/react-query'
+import { mutationOptions as original_mutationOptions } from '@tanstack/react-query'
 
 /**
- * @experimental This is experimental feature.
+ * This feature is officially supported in \@tanstack/react-query@5, You can proceed with the migration.
+ * @deprecated Use `mutationOptions` from \@tanstack/react-query@5
+ * @example
+ * ```diff
+ * - import { mutationOptions } from '@suspensive/react-query'
+ * + import { mutationOptions } from '@tanstack/react-query'
+ * ```
  */
-export function mutationOptions<TData = unknown, TError = DefaultError, TVariables = void, TContext = unknown>(
-  options: UseMutationOptions<TData, TError, TVariables, TContext>
-): UseMutationOptions<TData, TError, TVariables, TContext> {
-  return options
-}
+export const mutationOptions = original_mutationOptions
