e34d36810d957937e11bf42c0a37b98458663a30
Wonsuk Choi <sukvvon@gmail.com>
2025-09-25 00:24:27 +0900

    chore(eslint-config): update 'typescript-eslint' to v8.43.0 and replace deprecated 'tseslint.config' with 'defineConfig' (#1733)
    Co-authored-by: Jonghyeon Ko <jonghyeon@toss.im>

--- configs/eslint-config/index.js
+++ configs/eslint-config/index.js
@@ -1,4 +1,5 @@
 import pluginReact from '@eslint-react/eslint-plugin'
+import { defineConfig } from 'eslint/config'
 import importPlugin from 'eslint-plugin-import'
 import jsdoc from 'eslint-plugin-jsdoc'
 import reactHooks from 'eslint-plugin-react-hooks'
@@ -12,7 +13,7 @@ import * as mdx from 'eslint-plugin-mdx'
 
 const ignores = ['**/.next/**', '**/build/**', '**/coverage/**', '**/dist/**']
 
-export const suspensiveTypeScriptConfig = tseslint.config(
+export const suspensiveTypeScriptConfig = defineConfig(
   {
     ignores,
   },
@@ -75,6 +76,7 @@ export const suspensiveTypeScriptConfig = tseslint.config(
       '@typescript-eslint/only-throw-error': 'warn',
       '@typescript-eslint/no-confusing-void-expression': 'off',
       '@typescript-eslint/restrict-template-expressions': 'off',
+      '@typescript-eslint/no-deprecated': 'warn',
       '@typescript-eslint/no-empty-function': 'warn',
       '@typescript-eslint/no-extra-semi': 'warn',
       '@typescript-eslint/no-empty-interface': 'warn',
@@ -110,7 +112,7 @@ export const suspensiveTypeScriptConfig = tseslint.config(
   eslintPluginPrettierRecommended
 )
 
-export const suspensiveReactTypeScriptConfig = tseslint.config(
+export const suspensiveReactTypeScriptConfig = defineConfig(
   ...suspensiveTypeScriptConfig,
   reactHooks.configs.recommended,
   {
