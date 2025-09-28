59486721c816f07d7c93a30926ef8be071847b23
minho jang <mjang6988@gmail.com>
2025-09-29 03:33:12 +0900

    refactor: improve genericForwardRef function type safety

--- packages/react/src/utils/genericForwardRef.tsx
+++ packages/react/src/utils/genericForwardRef.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import { type PropsWithoutRef, type ReactElement, type Ref, type RefAttributes, forwardRef } from 'react'
 
 /**
  * A utility that allows creating a forwardRef component that can accept generic props.
  * React.forwardRef does not support this out of the box.
  */
-export function genericForwardRef<TRef, TProp = {}>(
-  render: (props: TProp, ref: React.Ref<TRef>) => React.ReactElement | null
-): (props: TProp & React.RefAttributes<TRef>) => React.ReactElement | null {
-  return React.forwardRef(render as any) as any
+export function genericForwardRef<TRef, TProp = object>(
+  render: (props: PropsWithoutRef<TProp>, ref: Ref<TRef>) => ReactElement | null
+) {
+  return forwardRef(render) as (props: TProp & RefAttributes<TRef>) => ReactElement | null
 }
