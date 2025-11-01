9c1462705d7d340ec5bf3025151178c9881531df
Jonghyeon Ko <jonghyeon@toss.im>
2025-11-01 20:38:38 +0900

    chore: update package description and mark features as experimental
    - Changed package description in package.json to "Experimental".
    - Added experimental annotations to nextComponentType and headers in index.ts and react-server.ts files.

--- packages/next/src/index.ts
+++ packages/next/src/index.ts
@@ -2,11 +2,17 @@
 
 import type { headers as nextHeaders } from 'next/headers'
 
+/**
+ * @experimental This is experimental feature.
+ */
 export const nextComponentType = ():
   | 'React Server Component'
   | 'React Client Component (browser)'
   | 'React Client Component (server)' =>
   typeof window === 'undefined' ? 'React Client Component (server)' : 'React Client Component (browser)'
 
+/**
+ * @experimental This is experimental feature.
+ */
 export const headers: typeof nextHeaders = (() =>
   Promise.resolve<undefined>(undefined)) as unknown as typeof nextHeaders
