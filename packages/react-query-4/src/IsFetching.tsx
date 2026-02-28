6ab4e24998e5b766a0bbbfbf417b1a4f77f188ea
saengmotmi <ohjtack@gmail.com>
2026-03-01 06:01:22 +0900

    fix(react-query-4): forward filters in IsFetching

--- packages/react-query-4/src/IsFetching.tsx
+++ packages/react-query-4/src/IsFetching.tsx
@@ -4,10 +4,12 @@ import { type QueryFilters, useIsFetching } from '@tanstack/react-query'
 import type { ReactNode } from 'react'
 
 /**
+ * Renders the current fetching count for queries that match the given filters.
  * @experimental This is an experimental feature.
  */
 export const IsFetching = ({
   children,
+  ...filter
 }: QueryFilters & { children: (isFetching: ReturnType<typeof useIsFetching>) => ReactNode }) => (
-  <>{children(useIsFetching())}</>
+  <>{children(useIsFetching(filter))}</>
 )
