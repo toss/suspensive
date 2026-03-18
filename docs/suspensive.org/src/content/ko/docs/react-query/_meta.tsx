f8e2bcfb673e09994762240048cae33669fa5641
Jonghyeon Ko <jonghyeon@toss.im>
2026-03-18 10:27:36 +0900

    fix(docs): standardize section titles and improve clarity across multiple files (#1926)

--- docs/suspensive.org/src/content/ko/docs/react-query/_meta.tsx
+++ docs/suspensive.org/src/content/ko/docs/react-query/_meta.tsx
@@ -2,7 +2,7 @@ import type { MetaRecord } from 'nextra'
 import type MetaEn from '../../../en/docs/react-query/_meta'
 
 export default {
-  motivation: { title: '어떤 이유로 사용하나요?' },
+  motivation: { title: '동기' },
   installation: { title: '설치하기' },
   'tanstack-query-compatibility': {
     title: 'TanStack Query v4, 5를 모두 지원',
